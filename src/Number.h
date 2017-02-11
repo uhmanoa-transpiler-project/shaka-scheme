cb11e88c93ad92fef2513c92e47fee336e6bc4b5
Troy Wooton <gcowootont@gmail.com>
2017-02-10 14:06:05 -1000

    Implemented Numeric predicates for properties

--- src/Number.h
+++ src/Number.h
@@ -8,36 +8,39 @@ class Number {
 public:
 
 	using Value = double;
-
+	
+	// default and initialization constructors
 	Number() : value(0) {}	
 	Number(Value v) : value(v) {}
 	
 
 	Value get_Value() {return value;}
-	// arithmetic operators
+	// arithmetic operators R7RS 6.2.6
 	friend Number operator+(const Number& n1, const Number& n2);
 	friend Number operator-(const Number& n1, const Number& n2);
 	friend Number operator*(const Number& n1, const Number& n2);
 	friend Number operator/(const Number& n1, const Number& n2);
-
-	// comparison operators
+	
+	// comparison operators R7RS 6.2.6
 	friend bool operator==(const Number& n1, const Number& n2);
 	friend bool operator!=(const Number& n1, const Number& n2);
 	friend bool operator>(const Number& n1, const Number& n2);
 	friend bool operator<(const Number& n1, const Number& n2);
 	friend bool operator>=(const Number& n1, const Number& n2);
 	friend bool operator<=(const Number& n1, const Number& n2);
-
-
+	
+	// testing for exactness/inexactness R7RS 6.2.2
 	friend bool exact_p(const Number& n);
 	friend bool inexact_p(const Number& n);
-	/* 
+
+	// numerical predicates testing a number for a particular property R7RS 6.2.6
 	friend bool zero_p(const Number& n);
 	friend bool positive_p(const Number& n);
 	friend bool negative_p(const Number& n);
 	friend bool odd_p(const Number& n);
 	friend bool even_p(const Number& n);
-
+	
+	/*
 	friend Number max(const Number& n1, const Number& n2);
 	friend Number min(const Number& n1, const Number& n2);
 
@@ -112,6 +115,29 @@ bool exact_p(const Number& n) {
 bool inexact_p(const Number& n) {
 	return !(exact_p(n));
 }
+
+bool zero_p(const Number& n) {
+	return (n.value == 0);
+}
+
+bool positive_p(const Number& n) {
+	return (n.value > 0);
+}
+
+bool negative_p(const Number& n) {
+	return (n.value < 0);
+}
+
+bool even_p(const Number& n) {
+	return (n.value % 2 == 0);
+}
+
+bool odd_p(const Number& n) {
+	return !(even_p(n));
+}
+
+
+
 } // namespace shaka
 
 #endif // NUMBER_H
