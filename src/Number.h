76b0aee27379242bfaeda44d9dd2b187b9f3839a
Troy Wooton <gcowootont@gmail.com>
2017-02-14 17:02:26 -1000

    Added implementation of min and max procedures

--- src/Number.h
+++ src/Number.h
@@ -40,10 +40,11 @@ public:
 	friend bool odd_p(const Number& n);
 	friend bool even_p(const Number& n);
 	
-	/*
+	// max and min of two numbers R7RS 6.2.6
 	friend Number max(const Number& n1, const Number& n2);
 	friend Number min(const Number& n1, const Number& n2);
-
+	
+	/*
 	friend Number abs(const Number& n);
 
 	friend Number floor(const Number& n);
@@ -144,7 +145,27 @@ bool odd_p(const Number& n) {
 	return !(even_p(n));
 }
 
+//------------------------------------------------
+// Max and Min of two Numbers: Built-in
+//------------------------------------------------
 
+Number max(const Number& n1, const Number& n2) {
+	if (n1 >= n2) {
+		return n1;
+	}
+	else {
+		return n2;
+	}
+}
+
+Number min(const Number& n1, const Number& n2) {
+	if (n1 <= n2) {
+		return n1;
+	}
+	else {
+		return n2;
+	}
+}
 
 } // namespace shaka
 
