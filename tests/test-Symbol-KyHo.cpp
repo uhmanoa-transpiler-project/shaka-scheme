8e97de1a1529056dbb015bcfda3088f3785ff80d
Ky Ho <kyho35@hawaii.edu>
2017-02-15 17:25:32 -1000

    Added symbol.h with test cases

---
+++ tests/test-Symbol-KyHo.cpp
@@ -0,0 +1,22 @@
+#include "gtest/gtest.h"
+#include "Symbol.h" 
+
+TEST(Symbol, a_symbol){
+	shaka::Symbol n1("symbol");
+	shaka::Symbol n2("burrito");
+
+	ASSERT_EQ(n1 == n2, false);
+}
+
+TEST(Symbol, check_equal_symbol){
+	shaka::Symbol n1("bird");
+	shaka::Symbol n2("bird");
+
+	ASSERT_EQ(n1 == n2, true);
+}
+
+int main(int argc, char** argv){
+	::testing::InitGoogleTest(&argc, argv);
+
+	return RUN_ALL_TESTS();
+}
