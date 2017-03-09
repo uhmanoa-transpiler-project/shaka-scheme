42b3db2524f1ba17a7c8700a98d047c88d415b2c
VermillionAzure <atasato@hawaii.edu>
2017-03-08 16:13:29 -1000

    Added Eval_Expression and updated EvaluatorStrategies

---
+++ src/Eval_Expression.h
@@ -0,0 +1,31 @@
+#ifndef EVAL_QUOTE_H
+#define EVAL_QUOTE_H
+#include "IEvaluatorStrategy.h"
+#include "EvaluationStrategies.h"
+
+namespace shaka {
+
+/// Encapsulates the EvaluatorStrategy classes
+namespace eval {
+
+/// @brief returns a std::shared_ptr to the child node
+template <typename T, typename Key, typename Value > 
+class Expression : public shaka::IEvaluatorStrategy<T, Key, Value> {
+
+	std::shared_ptr<IDataNode<T>> evaluate(std::shared_ptr<IDataNode<T>> node,
+			std::shared_ptr<IEnvironment<Key, Value>> env) {
+
+        if (node->get_data(0)->type() == typeid(shaka::Symbol)) {
+
+        }
+		
+
+	}
+
+};
+
+} // namespace eval
+
+} // namespace shaka
+
+#endif // EVAL_QUOTE_H
