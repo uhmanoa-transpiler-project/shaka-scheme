8807b7bffdfcb19c75e5125bfd2c29b19f3506c1
VermillionAzure <atasato@hawaii.edu>
2017-03-08 17:29:29 -1000

    Working on Procedure test case running

--- src/Eval_Quote.h
+++ src/Eval_Quote.h
@@ -10,7 +10,7 @@ namespace eval {
 /// @brief returns a std::shared_ptr to the child node
 template <typename T, typename Key, typename Value > 
 class Quote : public shaka::IEvaluatorStrategy<T, Key, Value> {
-
+public:
 	std::shared_ptr<IDataNode<T>> evaluate(std::shared_ptr<IDataNode<T>> node,
 			std::shared_ptr<IEnvironment<Key, Value>> env) {
 		return node->get_child(0);
