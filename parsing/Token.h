a0cdbe3b84173b4588aae6f3ed419fd5a213c983
VermillionAzure <atasato@hawaii.edu>
2017-03-13 13:42:22 -1000

    Split into Token/izer.h files

---
+++ parsing/Token.h
@@ -0,0 +1,56 @@
+#ifndef SHAKA_PARSER_TOKEN_H
+#define SHAKA_PARSER_TOKEN_H
+
+#include <string>
+
+namespace shaka {
+
+struct Token {
+public:
+    enum class Type : int {
+        IDENTIFIER,
+        BOOLEAN_TRUE,
+        BOOLEAN_FALSE,
+        NUMBER,
+        CHARACTER,
+        STRING,
+        PAREN_START,
+        PAREN_END,
+        BYTEVECTOR_START,
+        U8_BYTEVECTOR_START,
+        QUOTE,
+        BACKTICK,
+        COMMA,
+        COMMA_ATSIGN,
+        PERIOD,
+        DATUM_COMMENT,
+        COMMENT_START,
+        COMMENT_END,
+        DIRECTIVE,
+        END_OF_FILE
+    };
+
+    Token::Type type;
+    std::string str;
+
+    Token(Token::Type type) :
+        type(type),
+        str("") {}
+
+    Token(Token::Type type, const std::string& str) :
+        type(type),
+        str(str) {}
+
+    bool operator== (const Token& other) {
+        return (this->type == other.type &&
+                this->str  == other.str);
+    }
+
+    bool operator!= (const Token& other) {
+        return !(operator==(other));
+    }
+};
+
+} // namespace shaka
+
+#endif // SHAKA_PARSER_TOKEN_H
