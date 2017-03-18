c0d16ed86f3dd494b83e9ba5ef0318fa9ce807ef
Paulo L <plemus@hawaii.edu>
2017-03-17 20:10:50 -1000

    Finalized testcase and examples for the tree population from rule_define and the tree evaluation from the Evaluator

--- tests/test-Tokenizer-define.cpp
+++ tests/test-Tokenizer-define.cpp
@@ -2,6 +2,10 @@
 #include "Data.h"
 #include "DataNode.h"
 #include "IDataNode.h"
+#include "Environment.h"
+#include "Evaluator.h"
+#include "Eval_Define.h"
+
 #include "parser/Tokenizer.h"
 #include "parser/Token.h"
 #include "parser/rule_define.h"
@@ -19,6 +23,7 @@ TEST(Tokenizer_define, token_constructor) {
     );
 }
 
+/// @brief Assert that two tokens are equal
 TEST(Tokenizer_define, tokenizer_test) {
     // Create a stringstream and give it to the Tokenizer
     std::stringstream ss("( define\nasdf\t1    )");
@@ -30,6 +35,8 @@ TEST(Tokenizer_define, tokenizer_test) {
     ASSERT_EQ(tok1, tok2);
 }
 
+/// @brief Assert that rule_define can parse the base 
+/// of (define asdf 1)
 TEST(Tokenizer_define, define_return) {
 
     std::stringstream ss("( define asdf 1 )");
@@ -40,6 +47,8 @@ TEST(Tokenizer_define, define_return) {
     std::cout << "interm: " << interm << std::endl;
 }
 
+/// @brief Asser that rule_define can parse a slightly
+/// more complicated version of define using extra whitespace
 TEST(Tokenizer_define, define_number) {
 
     std::stringstream ss(" (   define \n true \n1 \n )");
@@ -50,6 +59,8 @@ TEST(Tokenizer_define, define_number) {
     std::cout << "interm: " << interm << std::endl;
 }
 
+/// @brief Test that rule_define can parse a boolean as the 
+/// expression
 TEST(Tokenizer_define, define_bool) {
     
     std::stringstream ss("(define true #true)");
@@ -70,22 +81,34 @@ TEST(Tokenizer_define, define_fail) {
     std::cout << "interm: " << interm << std::endl;
 }
 
+////////////////////////////////
+//        TREE TESTS          //
+////////////////////////////////
+
+using Data = shaka::Data;
+using IDataTree = shaka::IDataNode<Data>;
+using DataTree  = shaka::DataNode<Data>;
+using Environment = shaka::Environment<shaka::Symbol, std::shared_ptr<IDataTree>>;
+
 TEST(Tokenizer_define, define_tree) {
     
     std::stringstream ss("(define x 1)");
     shaka::Tokenizer in(ss);
     std::string interm;
 
-    std::shared_ptr<shaka::DataNode<shaka::Data>> root = 
-        std::make_shared<shaka::DataNode<shaka::Data>>(shaka::MetaTag::DEFINE);
+    std::shared_ptr<DataTree> root = std::make_shared<DataTree>(shaka::MetaTag::LIST);
+    std::shared_ptr<Environment> env = std::make_shared<Environment>(nullptr);
 
     ASSERT_TRUE( shaka::parser::rule::define(in, root, interm) );
-    
+
+    shaka::Evaluator evaluator(root->get_child(0), env);
+    evaluator.evaluate(shaka::eval::Define());
+
     ASSERT_EQ( root->get_num_children(), 1 );
     auto child = root->get_child(0);
-
     ASSERT_EQ( child->get_num_children(), 2 );
-
+    ASSERT_EQ(typeid(shaka::Number), env->get_value(shaka::Symbol("x"))->get_data()->type());
+    ASSERT_EQ(shaka::Number(1), shaka::get<shaka::Number>(*env->get_value(shaka::Symbol("x"))->get_data()));
 }
 
 int main(int argc, char** argv) {
