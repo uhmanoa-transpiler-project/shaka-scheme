d298b45ea16b739de05454e6092ae9913ed2bc83
VermillionAzure <atasato@hawaii.edu>
2017-03-22 18:10:45 -1000

    Added skeletons for standard procedures

---
+++ src/stdproc/Numbers_Arithmetic_impl.h
@@ -0,0 +1,58 @@
+#ifndef SHAKA_STDPROC_NUMBERS_ARITHMETIC_IMPL_H
+#define SHAKA_STDPROC_NUMBERS_ARITHMETIC_IMPL_H
+
+// (+ z1 ...)
+// (* z1 ...)
+// (- z)
+// (- z1 z2 ...)
+// (/ z1)
+// (/ z1 z2 ...)
+
+// (abs x)
+// (floor/ n1 n2)
+// (floor-quotient n1 n2)
+// (floor-remainder n1 n2)
+// (truncate/ n1 n2)
+// (truncate-quotient n1 n2)
+// (truncate-remainder n1 n2)
+
+// (quotient n1 n2)
+// (remainder n1 n2)
+// (modulo n1 n2)
+
+// (gcd n1 ...)
+// (lcm n1 ...)
+
+// (numerator q)
+// (denominator q)
+
+// (floor x)
+// (ceiling x)
+// (truncate x)
+// (round x)
+
+// (rationalize x y)
+
+// (exp z)
+// (log z)
+// (log z1 z2)
+// (sin z)
+// (cos z)
+// (tan z)
+// (asin z)
+// (acos z)
+// (atan z)
+// (atan y x)
+
+// (square z)
+// (sqrt z)
+
+// (exact-integer-sqrt k)
+// (expt z1 z2)
+
+// DON'T DO THE COMPLEX LIBRARY PROCEDURES
+
+// (inexact z)
+// (exact z)
+
+#endif // SHAKA_STDPROC_NUMBERS_ARITHMETIC_IMPL_H
