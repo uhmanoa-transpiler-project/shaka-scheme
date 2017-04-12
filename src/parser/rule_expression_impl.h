0a87a524dea1b58f25a1edcf86115b5cedfeab90
Paulo L <plemus@hawaii.edu>
2017-04-11 20:48:48 -1000

    Made improvements to expression

--- src/parser/rule_expression_impl.h
+++ src/parser/rule_expression_impl.h
@@ -38,10 +38,63 @@ bool expression (
         // First, I want to check for any rules that may be recursive
         // or that may include any of the smaller rules such as literal.
         //
-        // Things that start with '(':
+        // Things that can start with '(':
         //      quotation
-        if(in.peek().type == shaka::Token::Type::PAREN_START)
+        // Things that must start with '(':
+        //      procedure call: (<operator> ......
+        //      lambda        : (lambda .......
+        //      conditional   : (if ........
+        //
+        //  Required look ahead.
+        if(in.peek().type == shaka::Token::Type::PAREN_START) {
+            
+        }
 
+        // Pretty much can only be quotation
+        else if(in.peek().type == shaka::Token::Type::QUOTE) {
+            
+        }
+
+        // Covers NUMBER
+        // This turns in to a shaka::NUMBER data
+        else if(in.peek().type == shaka::Token::Type::NUMBER) {
+
+            tokens.push(in.get());
+            interm += tokens.top().get_string();
+
+            if(root != nullptr)
+                root->push_child(
+                        shaka::Number( std::stod(tokens.top().get_string() )
+                            ));
+            
+
+        }
+
+        // Covers.......
+        //  
+        //      identifier
+        //      boolean
+        //      character
+        //      string
+        //
+        // Place these directly as children of the root as shaka::SYMBOL
+        else if(in.peek().type == shaka::Token::Type::IDENTIFIER    ||
+                in.peek().type == shaka::Token::Type::BOOLEAN_TRUE  ||
+                in.peek().type == shaka::Token::Type::BOOLEAN_FALSE ||
+                in.peek().type == shaka::Token::Type::CHARACTER     ||
+                in.peek().type == shaka::Token::Type::STRING) {
+
+            tokens.push(in.get());
+            interm += tokens.top().get_string();
+
+            // Add to tree
+            if(root != nullptr)
+                root->push_child(shaka::Symbol(tokens.top().get_string()));
+
+        }
+        else throw std::runtime_error("EXPRESSION: Got to else, no conditions met");
+
+        return true;
 
     } catch(std::runtime_error& e) {
 
