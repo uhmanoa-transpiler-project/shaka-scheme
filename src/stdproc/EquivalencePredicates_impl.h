e393dfa0ce86000c67cfeca473d7ab87c2af4653
Anthony Martin <amartin3@hawaii.edu>
2017-04-12 14:56:00 -1000

    Added the eq and equal functions

--- src/stdproc/EquivalencePredicates_impl.h
+++ src/stdproc/EquivalencePredicates_impl.h
@@ -83,6 +83,134 @@ else{
 }
 Function eqv = equivalent;
 /// (eq? obj1 obj2)
+
+Args equivalence(Args a){
+shaka::Boolean c(true);
+shaka::Boolean d(false);
+if((*a[0]->get_data()).type() == typeid(shaka::Number) && (*a[1]->get_data()).type() == typeid(shaka::Number)){
+	if(*a[0]->get_data() == *a[1]->get_data())
+		return{std::make_shared<shaka::DataNode<shaka::Data>>
+			((c))};
+	else{
+
+		return{std::make_shared<shaka::DataNode<shaka::Data>>
+			((d))};
+	}
+}
+
+if((*a[0]->get_data()).type() == typeid(shaka::Symbol) && (*a[1]->get_data()).type() == typeid(shaka::Symbol)){
+	if(*a[0]->get_data() == *a[1]->get_data())
+		return{std::make_shared<shaka::DataNode<shaka::Data>>
+			((c))};
+	else{
+
+		return{std::make_shared<shaka::DataNode<shaka::Data>>
+			((d))};
+	}
+}
+if((*a[0]->get_data()).type() == typeid(shaka::String) && (*a[1]->get_data()).type() == typeid(shaka::String)){
+	if(*a[0]->get_data() == *a[1]->get_data())
+		return{std::make_shared<shaka::DataNode<shaka::Data>>
+			((c))};
+	else{
+
+		return{std::make_shared<shaka::DataNode<shaka::Data>>
+			((d))};
+	}
+}
+
+if((*a[0]->get_data()).type() == typeid(shaka::Boolean) && (*a[1]->get_data()).type() == typeid(shaka::Boolean)){
+	if(*a[0]->get_data() == *a[1]->get_data())
+		return{std::make_shared<shaka::DataNode<shaka::Data>>
+			((c))};
+	else{
+
+		return{std::make_shared<shaka::DataNode<shaka::Data>>
+			((d))};
+	}
+}
+
+if((*a[0]->get_data()).type() == typeid(shaka::MetaTag::LIST) && (*a[1]->get_data()).type() == typeid(shaka::MetaTag::LIST)){
+	if(a[0]->get_num_children() == 0 && a[1]->get_num_children() == 0)
+		return{std::make_shared<shaka::DataNode<shaka::Data>>
+			((c))};
+	else{
+
+		return{std::make_shared<shaka::DataNode<shaka::Data>>
+			((d))};
+	}
+}
+else{	
+	return{std::make_shared<shaka::DataNode<shaka::Data>>
+		((d))};
+}
+
+}
+Function eq = equivalence;
 /// (equal? obj1 obj2)
+
+Args equality(Args a){
+shaka::Boolean c(true);
+shaka::Boolean d(false);
+if((*a[0]->get_data()).type() == typeid(shaka::Number) && (*a[1]->get_data()).type() == typeid(shaka::Number)){
+	if(*a[0]->get_data() == *a[1]->get_data())
+		return{std::make_shared<shaka::DataNode<shaka::Data>>
+			((c))};
+	else{
+
+		return{std::make_shared<shaka::DataNode<shaka::Data>>
+			((d))};
+	}
+}
+
+if((*a[0]->get_data()).type() == typeid(shaka::Symbol) && (*a[1]->get_data()).type() == typeid(shaka::Symbol)){
+	if(*a[0]->get_data() == *a[1]->get_data())
+		return{std::make_shared<shaka::DataNode<shaka::Data>>
+			((c))};
+	else{
+
+		return{std::make_shared<shaka::DataNode<shaka::Data>>
+			((d))};
+	}
+}
+if((*a[0]->get_data()).type() == typeid(shaka::String) && (*a[1]->get_data()).type() == typeid(shaka::String)){
+	if(*a[0]->get_data() == *a[1]->get_data())
+		return{std::make_shared<shaka::DataNode<shaka::Data>>
+			((c))};
+	else{
+
+		return{std::make_shared<shaka::DataNode<shaka::Data>>
+			((d))};
+	}
+}
+
+if((*a[0]->get_data()).type() == typeid(shaka::Boolean) && (*a[1]->get_data()).type() == typeid(shaka::Boolean)){
+	if(*a[0]->get_data() == *a[1]->get_data())
+		return{std::make_shared<shaka::DataNode<shaka::Data>>
+			((c))};
+	else{
+
+		return{std::make_shared<shaka::DataNode<shaka::Data>>
+			((d))};
+	}
+}
+
+if((*a[0]->get_data()).type() == typeid(shaka::MetaTag::LIST) && (*a[1]->get_data()).type() == typeid(shaka::MetaTag::LIST)){
+	if(a[0]->get_num_children() == 0 && a[1]->get_num_children() == 0)
+		return{std::make_shared<shaka::DataNode<shaka::Data>>
+			((c))};
+	else{
+
+		return{std::make_shared<shaka::DataNode<shaka::Data>>
+			((d))};
+	}
+}
+else{	
+	return{std::make_shared<shaka::DataNode<shaka::Data>>
+		((d))};
+}
+
+}
+Function equal = equality;
 
 #endif // SHAKA_STDPROC_EQUIVALENCE_PREDICATES_IMPL_H
