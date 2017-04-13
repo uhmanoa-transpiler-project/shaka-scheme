88e5124c7e59aefdd1a597c2b4086d3ca15598ba
Ky Ho <kyho35@hawaii.edu>
2017-04-12 16:50:04 -1000

    New Structure for Core-Systems & Salvaged Code

---
+++ src/core/base/Symbol.h
@@ -0,0 +1,42 @@
+#ifndef SYMBOL_H
+#define SYMBOL_H
+#include <boost/flyweight.hpp>
+#include <string>
+
+namespace shaka{
+
+
+class Symbol {
+public:
+	using Value = std::string;
+
+	//TODO implement interface
+	Symbol(Value v) : value(v){}
+
+	Value get_value() {return value;}	
+	// overload the == and != operator for Symbol
+	friend bool operator==(const Symbol& n1, const Symbol& n2);
+	// hint: == is already overloaded for std::string
+	// != can be defined in terms of ==
+	friend bool operator!=(const Symbol& n1, const Symbol& n2);
+	friend bool operator<(const Symbol& n1, const Symbol& n2);
+
+private:
+	std::string value;
+};
+
+bool operator<(const Symbol& n1, const Symbol& n2){
+	return n1.value[0] < n2.value[0];
+}
+
+
+bool operator==(const Symbol& n1, const Symbol& n2){
+	return n1.value == n2.value;
+}
+
+bool operator!=(const Symbol& n1, const Symbol& n2){
+	return !(n1 == n2);
+}
+
+}
+#endif
