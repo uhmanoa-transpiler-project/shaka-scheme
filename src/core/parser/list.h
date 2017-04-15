065bb51a6a9959a9bf03465303de625692edb73e
paulolemus <paulolemus14@gmail.com>
2017-04-15 12:41:36 -1000

    Wrote a test case for Quote modelling the recursive call for the list function, set up prototype for functions and wrote comments saying what we plan on doing

---
+++ src/core/parser/list.h
@@ -0,0 +1,22 @@
+#ifndef SHAKA_CORE_PARSER_LIST_H_
+#define SHAKA_CORE_PARSER_LIST_H_
+
+#include "core/parser/primitives.h"
+
+namespace shaka {
+namespace parser {
+
+// Function that may be recursively called which 
+// populated any nested lists and returns them.
+DataNode list(
+    InputStream&    in
+) {
+
+
+}
+
+
+} // parser
+} // shaka
+
+#endif // SHAKA_CORE_PARSER_LIST_H_
