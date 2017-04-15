ad698c831024259e9423ff9074528989a834afaf
paulolemus <paulolemus14@gmail.com>
2017-04-15 11:04:45 -1000

    Set up skeletons, moved tokenizer over

--- src/core/parser/rule_define.h
+++ src/core/parser/rule_define.h
@@ -1,5 +1,16 @@
-#ifndef SHAKA_CORE_PARSER_RULE_DEFINE_H_
-#define SHAKA_CORE_PARSER_RULE_DEFINE_H_
+#ifndef SHAKA_CORE_PARSER_DEFINE_H_
+#define SHAKA_CORE_PARSER_DEFINE_H_
 
+#include "core/base/Data.h"
+
+namespace shaka {
+namespace parser {
+
+
+bool define();
+
+
+} // namespace parser
+} // namespace shaka
 
 #endif // SHAKA_CORE_PARSER_RULE_DEFINE_H_
