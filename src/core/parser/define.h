aafd2879fed8b0c66c1f96a73145360005ff080f
paulolemus <paulolemus14@gmail.com>
2017-04-15 11:18:28 -1000

    Set up skeletons AGAIN

--- src/core/parser/define.h
+++ src/core/parser/define.h
@@ -1,6 +1,14 @@
 #ifndef SHAKA_CORE_PARSER_DEFINE_H_
 #define SHAKA_CORE_PARSER_DEFINE_H_
 
+/* Contains Data, DataNode, Token, Tokenizer
+ * InputStream as Tokenizer
+ * ListPtr as std::shared_ptr<DataNode>
+ * NodePtr
+ * EnvPtr
+ */
+#include "core/parser/primitives.h"
+
 namespace shaka {
 namespace parser {
 
---
+++ src/core/parser/primitives.h
@@ -0,0 +1,18 @@
+#ifndef SHAKA_CORE_PARSER_PRIMITIVES_H_
+#define SHAKA_CORE_PARSER_PRIMITIVES_H_
+
+#include "core/base/DataNode.h"
+#include "core/parser/Tokenizer.h"
+
+namespace shaka {
+namespace parser {
+
+using InputStream = shaka::Tokenizer;
+
+using Char = char;
+
+
+} // parser
+} // shaka
+
+#endif // SHAKA_CORE_PARSER_PRIMITIVES_H_
