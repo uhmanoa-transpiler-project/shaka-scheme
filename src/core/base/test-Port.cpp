5fd757e572b754d69edbf132a4ae7ccf27986712
Glesa Tolentino <gmtolent@hawaii.edu>
2017-04-18 20:41:51 -1000

    Created more test cases for Port.h

--- src/core/base/test-Port.cpp
+++ src/core/base/test-Port.cpp
@@ -7,35 +7,105 @@
 
 using namespace shaka;
 
-TEST(Port, initialization) {
+//Text, input, filesteam -
+TEST(Port, initialization_text_input_filestream) {
 	std::string str = "sample";
-	//Port(std::string str, Mode mode, Type type, Source source)
-//	shaka::Port p0(str, shaka::Port::Mode::TEXT, shaka::Port::Type::INPUT, shaka::Port::Source::STRING);
-	shaka::Port p1(
-        str,
-        shaka::Port::Mode::TEXT,
-        shaka::Port::Type::INPUT,
-        shaka::Port::Source::FILESTREAM);
+	shaka::Port p1(str, shaka::Port::Mode::TEXT, shaka::Port::Type::INPUT, shaka::Port::Source::FILESTREAM);
 		
-//	shaka::Port p2(str, shaka::Port::Mode::TEXT, shaka::Port::Type::OUTPUT, shaka::Port::Source::STRING);
-//	shaka::Port p3(str, shaka::Port::Mode::TEXT, shaka::Port::Type::OUTPUT, shaka::Port::Source::FILESTREAM);
-	
-//	shaka::Port p3(shaka::Port::Mode::BINARY, shaka::Port::Type::INPUT);
-//	shaka::Port p4(str, shaka::Port::Mode::BINARY, shaka::Port::Type::OUTPUT, shaka::Port::Source::STRING);
-
 	//Check if mode and type is the sample
 	ASSERT_EQ(p1.get_mode(), shaka::Port::Mode::TEXT);
 	ASSERT_EQ(p1.get_type(), shaka::Port::Type::INPUT);
 	ASSERT_EQ(p1.get_source_type(), shaka::Port::Source::FILESTREAM);
+}
+
+//Text, input, string -
+TEST(Port, initialization_text_input_string) {
+	std::string str = "sample";
 	
-//	ASSERT_EQ(p4.get_mode(), shaka::Port::Mode::BINARY);
-//	ASSERT_EQ(p4.get_type(), shaka::Port::Type::OUTPUT);
-//	ASSERT_EQ(p4.get_source_type(), shaka::Port::Source::STRING);
+	//Port(std::string str, Mode mode, Type type, Source source)
+	shaka::Port p0(str, shaka::Port::Mode::TEXT, shaka::Port::Type::INPUT, shaka::Port::Source::STRING);
+
+	//Check if mode and type is the sample
+	ASSERT_EQ(p0.get_mode(), shaka::Port::Mode::TEXT);
+	ASSERT_EQ(p0.get_type(), shaka::Port::Type::INPUT);
+	ASSERT_EQ(p0.get_source_type(), shaka::Port::Source::STRING);
 }
 
-TEST(Port, file) {
+//Text, output, filestream -
+TEST(Port, initialization_text_output_filestream) {
+	std::string str = "sample";
 
+	shaka::Port p3(str, shaka::Port::Mode::TEXT, shaka::Port::Type::OUTPUT, shaka::Port::Source::FILESTREAM);
+	
+	ASSERT_EQ(p3.get_mode(), shaka::Port::Mode::TEXT);
+	ASSERT_EQ(p3.get_type(), shaka::Port::Type::OUTPUT);
+	ASSERT_EQ(p3.get_source_type(), shaka::Port::Source::FILESTREAM);
 }
+
+//Text, output, string -
+TEST(Port, initialization_text_output_string) {
+	std::string str = "sample";
+
+	shaka::Port p3(str, shaka::Port::Mode::TEXT, shaka::Port::Type::OUTPUT, shaka::Port::Source::STRING);
+	
+	ASSERT_EQ(p3.get_mode(), shaka::Port::Mode::TEXT);
+	ASSERT_EQ(p3.get_type(), shaka::Port::Type::OUTPUT);
+	ASSERT_EQ(p3.get_source_type(), shaka::Port::Source::STRING);
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+//Binary, input, filesteam -
+TEST(Port, initialization_binary_input_filestream) {
+	std::string str = "sample";
+	shaka::Port p1(str, shaka::Port::Mode::BINARY, shaka::Port::Type::INPUT, shaka::Port::Source::FILESTREAM);
+		
+	//Check if mode and type is the sample
+	ASSERT_EQ(p1.get_mode(), shaka::Port::Mode::BINARY);
+	ASSERT_EQ(p1.get_type(), shaka::Port::Type::INPUT);
+	ASSERT_EQ(p1.get_source_type(), shaka::Port::Source::FILESTREAM);
+}
+
+//Binary, input, string -
+TEST(Port, initialization_binary_input_string) {
+	std::string str = "sample";
+	
+	//Port(std::string str, Mode mode, Type type, Source source)
+	shaka::Port p0(str, shaka::Port::Mode::BINARY, shaka::Port::Type::INPUT, shaka::Port::Source::STRING);
+
+	//Check if mode and type is the sample
+	ASSERT_EQ(p0.get_mode(), shaka::Port::Mode::BINARY);
+	ASSERT_EQ(p0.get_type(), shaka::Port::Type::INPUT);
+	ASSERT_EQ(p0.get_source_type(), shaka::Port::Source::STRING);
+}
+
+//Binary, output, filestream -
+TEST(Port, initialization_binary_output_filestream) {
+	std::string str = "sample";
+
+	shaka::Port p3(str, shaka::Port::Mode::BINARY, shaka::Port::Type::OUTPUT, shaka::Port::Source::FILESTREAM);
+	
+	ASSERT_EQ(p3.get_mode(), shaka::Port::Mode::BINARY);
+	ASSERT_EQ(p3.get_type(), shaka::Port::Type::OUTPUT);
+	ASSERT_EQ(p3.get_source_type(), shaka::Port::Source::FILESTREAM);
+}
+
+//Binary, output, string -
+TEST(Port, initialization_binary_output_string) {
+	std::string str = "sample";
+
+	shaka::Port p3(str, shaka::Port::Mode::BINARY, shaka::Port::Type::OUTPUT, shaka::Port::Source::STRING);
+	
+	ASSERT_EQ(p3.get_mode(), shaka::Port::Mode::BINARY);
+	ASSERT_EQ(p3.get_type(), shaka::Port::Type::OUTPUT);
+	ASSERT_EQ(p3.get_source_type(), shaka::Port::Source::STRING);
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////
 
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
