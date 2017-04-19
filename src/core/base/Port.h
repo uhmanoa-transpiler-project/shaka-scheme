f29ba8a90b0c6bd1bfe2ea272537b9cfd1aec3b8
VermillionAzure <atasato@hawaii.edu>
2017-04-18 18:35:52 -1000

    Fixed header guard, added enums for Port

--- src/core/base/Port.h
+++ src/core/base/Port.h
@@ -1,14 +1,31 @@
-#ifndef PORT_H
+#ifndef SHAKA_CORE_BASE_PORT_H
 #define PORT_H
 
 #include <iostream>
 #include <exception>
 
 namespace shaka {
-  class Port;
-    enum class Mode;
-    enum class Type;
-  }
+
+class Port {
+public:
+    Port (Port::Mode mode,
+          Port::Type type) :
+        mode(mode),
+        type(type) {}
+
+    enum class Mode : int {
+        TEXT,
+        BINARY
+    };
+    enum class Type : int {
+        INPUT,
+        OUPTUT
+    };
+
+    Mode mode;
+    Type type;
+};
+
 }
 
 #endif
