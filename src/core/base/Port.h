e058aa1496ae840a069249cf26c9f03d9dc32622
VermillionAzure <atasato@hawaii.edu>
2017-04-18 19:35:52 -1000

    Finished draft of Port constructor for string/file

--- src/core/base/Port.h
+++ src/core/base/Port.h
@@ -45,38 +45,29 @@ public:
             modeflag |= std::fstream::binary;
         }
 
-
-        /*
         if (source == Source::FILESTREAM && type == Type::INPUT) {
             this->in = std::move(std::unique_ptr<std::istream>(
-                new std::ifstream(
-                   str.c_str(),
-                   modeflag 
-                )
-            ));
+                new std::ifstream(str.c_str(), modeflag)
+            )); 
         }
         else if (source == Source::FILESTREAM && type == Type::OUTPUT) {
             this->out = std::move(std::unique_ptr<std::ostream>(
-                new std::ofstream(
-                   str.c_str(),
-                   modeflag 
-                )
-            ));
+                new std::ofstream(str.c_str(), modeflag)
+            )); 
         }
-        else if (source == Source::STRING && type == Type::INPUT) { 
+        else if (source == Source::STRING && type == Type::INPUT) {
             this->in = std::move(std::unique_ptr<std::istream>(
-               new std::istringstream(str)
-            ));
+                new std::istringstream(str.c_str())
+            )); 
         }
         else if (source == Source::STRING && type == Type::OUTPUT) {
-            this->in = std::move(std::unique_ptr<std::ostream>(
-                new std::ostringstream(str)
-            ));
+            this->out = std::move(std::unique_ptr<std::ostream>(
+                new std::ostringstream(str.c_str())
+            )); 
         }
         else {
             throw std::runtime_error("Blah blah Port.h");
         }
-        */
     }
 
     Port::Mode get_mode() const {
