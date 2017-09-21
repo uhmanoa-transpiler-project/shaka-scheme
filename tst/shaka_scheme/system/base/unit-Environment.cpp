a6b7bfef0bdf5fb7858057bb57898ff670c4026d
dylannakahodo <dylannakahodo@gmail.com>
2017-09-20 17:38:46 -1000

    Started skeleton for unit test for Environment
    - Added skeleton unit test cases for Environment

---
+++ tst/shaka_scheme/system/base/unit-Environment.cpp
@@ -0,0 +1,22 @@
+//
+// Created by dylan on 9/20/17.
+//
+
+#include <gmock/gmock.h>
+#include "shaka_scheme/system/base/Environment.hpp"
+
+using namespace shaka;
+
+TEST(EnvironmentUnitTest, parent_constructor) { }
+TEST(EnvironmentUnitTest, get_parent) { }
+TEST(EnvironmentUnitTest, set_parent) { }
+TEST(EnvironmentUnitTest, set_value) { }
+TEST(EnvironmentUnitTest, get_value_success) { }
+TEST(EnvironmentUnitTest, get_value_invalid_key_failure) { }
+TEST(EnvironmentUnitTest, contains) { }
+TEST(EnvironmentUnitTest, is_defined) { }
+TEST(EnvironmentUnitTest, get_keys) { }
+TEST(EnvironmentUnitTest, get_bindings) { }
+
+
+
