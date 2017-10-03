228e5afbf785af9eb95147c2ca7e41cc2b4e911e
Billy Troy Wooton <gcowootont@gmail.com>
2017-10-02 22:28:42 -1000

    Created HeapVirtualMachine.cpp

---
+++ src/shaka_scheme/system/vm/HeapVirtualMachine.cpp
@@ -0,0 +1,61 @@
+//
+// Created by Billy Wooton on 10/2/17.
+//
+
+#include "shaka_scheme/system/vm/HeapVirtualMachine.hpp"
+
+namespace shaka {
+
+NodePtr HeapVirtualMachine::evaluate_assembly_instruction() {
+  shaka::DataPair& exp_pair = exp->get<DataPair>();
+  shaka::Symbol& instruction = exp_pair.car()->get<Symbol>();
+
+  if (instruction == shaka::Symbol("halt")) {
+    return this->acc;
+  }
+
+}
+
+Accumulator HeapVirtualMachine::get_accumulator() const {
+  return this->acc;
+}
+
+Expression HeapVirtualMachine::get_expression() const {
+  return this->exp;
+}
+
+EnvPtr HeapVirtualMachine::get_environment() const {
+  return this->env;
+}
+
+ValueRib HeapVirtualMachine::get_value_rib() const {
+  return this->rib;
+}
+
+FramePtr HeapVirtualMachine::get_call_frame() const {
+  return this->frame;
+}
+
+void HeapVirtualMachine::set_accumulator(Accumulator a) {
+  this->acc = a;
+}
+
+void HeapVirtualMachine::set_expression(Expression x) {
+  this->exp = x;
+}
+
+void HeapVirtualMachine::push_call_frame(FramePtr s) {
+  this->frame = s;
+}
+
+void HeapVirtualMachine::set_environment(EnvPtr e) {
+  this->env = e;
+}
+
+void HeapVirtualMachine::set_value_rib(ValueRib r) {
+  this->rib = r;
+}
+
+
+} //namespace shaka
+
