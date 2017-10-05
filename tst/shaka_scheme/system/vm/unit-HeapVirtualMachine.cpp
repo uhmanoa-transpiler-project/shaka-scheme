4c7cd4ab704243be0cc93968b6b7291d9bd02949
Billy Troy Wooton <gcowootont@gmail.com>
2017-10-04 16:23:58 -1000

    Wrote tests for assign, frame, argument, & return

--- tst/shaka_scheme/system/vm/unit-HeapVirtualMachine.cpp
+++ tst/shaka_scheme/system/vm/unit-HeapVirtualMachine.cpp
@@ -6,6 +6,7 @@
 #include "shaka_scheme/system/base/Data.hpp"
 #include "shaka_scheme/system/vm/HeapVirtualMachine.hpp"
 #include "shaka_scheme/system/base/Environment.hpp"
+#include "shaka_scheme/system/vm/CallFrame.hpp"
 #include "shaka_scheme/system/core/lists.hpp"
 
 using namespace shaka;
@@ -322,5 +323,318 @@ TEST(HeapVirtualMachineUnitTest, evaluate_test_then) {
 
   ASSERT_EQ(hvm.get_expression()->get<DataPair>().car()->get<Symbol>(),
             Symbol("halt"));
+
+}
+
+/**
+ * @brief Test: evaluate_assembly_instruction() with (assign var x) as exp
+ */
+TEST(HeapVirtualMachineUnitTest, evaluate_assign) {
+
+  // Given: An Accumulator with the Symbol("accumulator");
+
+  Accumulator acc = std::make_shared<Data>(Symbol("accumulator"));
+
+  // Given: An assembly instruction of the form (assign a (halt))
+  Symbol instruction("assign");
+  Data instruction_data(instruction);
+
+  Symbol halt("halt");
+  Data halt_data(halt);
+  DataPair halt_pair(halt_data);
+
+  Symbol var("a");
+  Data var_data(var);
+  DataPair var_halt_pair(var_data, *core::list(create_node(halt_pair)));
+
+  DataPair assign_var_halt(instruction_data, var_halt_pair);
+
+  NodePtr expression = std::make_shared<Data>(assign_var_halt);
+
+  // Given: An EnvPtr that points to an empty environment
+
+  EnvPtr env = std::make_shared<Environment>(nullptr);
+
+  // Given: An empty ValueRib
+
+  ValueRib vr;
+
+  // Given: A HeapVirtualMachine instance constructed with these items
+
+  HeapVirtualMachine hvm(acc, expression, env, vr, nullptr);
+
+  // When: You invoke the evaluate_assembly_instruction() method
+
+  hvm.evaluate_assembly_instruction();
+
+  // Then: The environment register will contain the binding [a : accumulator]
+
+  ASSERT_EQ(hvm.get_environment()->get_value(Symbol("a"))->get<Symbol>(),
+            Symbol("accumulator"));
+
+  // Then: The next expression will be (halt)
+
+  ASSERT_EQ(hvm.get_expression()->get<DataPair>().car()->get<Symbol>(),
+            Symbol("halt"));
+}
+
+/**
+ * @brief Test: evaluate_assembly_instruction() with (frame x ret) as exp
+ */
+TEST(HeapVirtualMachineUnitTest, evaluate_frame) {
+
+  // Given: An Accumulator containing the Symbol("finale")
+
+  Accumulator accumulator = std::make_shared<Data>(Symbol("finale"));
+
+  // Given: An assembly instruction of the form (frame (halt) (halt))
+
+  Symbol halt("halt");
+
+  Data halt_data(halt);
+
+  DataPair halt_pair(halt_data);
+
+  DataPair halt_halt(halt_pair, *core::list(create_node(halt_pair)));
+
+  Symbol frame("frame");
+  Data frame_data(frame);
+
+  DataPair frame_halt_halt(frame_data, halt_halt);
+
+  NodePtr expression = std::make_shared<Data>(frame_halt_halt);
+
+  // Given: An EnvPtr with bindings [a : "Hello", b : "World"]
+
+  EnvPtr env = std::make_shared<Environment>(nullptr);
+
+  env->set_value(Symbol("a"), std::make_shared<Data>(String("Hello")));
+  env->set_value(Symbol("b"), std::make_shared<Data>(String("World")));
+
+  // Given: A ValueRib containing strings "RVal1" "RVal2" "RVal3"
+
+  ValueRib vr = {create_node(String("RVal1")),
+                 create_node(String("RVal2")),
+                 create_node(String("RVal3"))};
+
+  // Given: A HeapVirtualMachine instance constructed with these items
+
+  HeapVirtualMachine hvm(accumulator, expression, env, vr, nullptr);
+
+  // When: You invoke the evaluate_assembly_instruction() method
+
+  hvm.evaluate_assembly_instruction();
+
+  // Then: The control stack register has a new CallFrame
+
+  ASSERT_NE(hvm.get_call_frame(), nullptr);
+
+  // Then: The new CallFrame has an environment with bindings for a and b
+
+  ASSERT_EQ(
+      hvm.get_call_frame()->
+          get_environment_pointer()->
+          get_value(Symbol("a"))->get<String>(),
+      String("Hello"));
+
+  ASSERT_EQ(
+    hvm.get_call_frame()->
+        get_environment_pointer()->
+        get_value(Symbol("b"))->get<String>(),
+    String("World")
+  );
+
+  // Then: The new CallFrame has a ValueRib with "RVal1" "RVal2" "RVal3"
+
+  ASSERT_EQ(
+    hvm.get_call_frame()->
+        get_value_rib()[0]->get<String>(),
+    String("RVal1")
+  );
+
+  ASSERT_EQ(
+    hvm.get_call_frame()->
+        get_value_rib()[1]->get<String>(),
+    String("RVal2")
+  );
+
+  ASSERT_EQ(
+    hvm.get_call_frame()->
+        get_value_rib()[2]->get<String>(),
+    String("RVal3")
+  );
+
+  // Then: The new CallFrame has a next expression that is (halt)
+
+  ASSERT_EQ(hvm.get_call_frame()->
+      get_next_expression()->
+      get<DataPair>().car()->get<Symbol>(),
+  Symbol("halt"));
+
+  // Then: The new CallFrame has a next reference that is nullptr
+
+  ASSERT_EQ(hvm.get_call_frame()->get_next_frame(), nullptr);
+
+  // Then: The HVM instance will have an empty ValueRib register
+
+  ASSERT_EQ(hvm.get_value_rib().size(), 0);
+
+  // Then: The HVM instance's Expression register will contain (halt)
+
+  ASSERT_EQ(hvm.get_expression()->get<DataPair>().car()->get<Symbol>(),
+  Symbol("halt"));
+
+  // When: You invoke the evaluate_assembly_instruction() method again
+
+  auto result = hvm.evaluate_assembly_instruction();
+
+  // Then: It will return the contents of the Accumulator
+
+  ASSERT_EQ(result->get<Symbol>(), Symbol("finale"));
+
+}
+
+/**
+ * @brief Test: evaluate_assembly_instruction() with (argument x) as exp
+ */
+TEST(HeapVirtualMachineUnitTest, evaluate_argument) {
+
+  // Given: An Accumulator containing the symbol 'banana
+
+  Symbol banana("banana");
+
+  Accumulator accumulator = create_node(banana);
+
+  // Given: An assembly instruction of the form (argument (halt))
+
+  Symbol halt("halt");
+  Data halt_data(halt);
+  DataPair halt_pair(halt_data);
+
+  Symbol argument("argument");
+  Data argument_data(argument);
+  DataPair argument_halt(argument, *core::list(create_node(halt_pair)));
+
+  NodePtr expression = create_node(argument_halt);
+
+  // Given: An EnvPtr to an empty Environment
+
+  EnvPtr env = std::make_shared<Environment>(nullptr);
+
+  // Given: An empty ValueRib
+
+  ValueRib vr;
+
+  // Given: An instance of the VM constructed with these items
+
+  HeapVirtualMachine hvm(accumulator, expression, env, vr, nullptr);
+
+  // When: You invoke the evaluate_assembly_instruction() method on hvm
+
+  hvm.evaluate_assembly_instruction();
+
+  // Then: The ValueRib register will hold the Symbol("banana");
+
+  ASSERT_EQ(hvm.get_value_rib()[0]->get<Symbol>(), Symbol("banana"));
+
+  // Then: The Expression register will hold the instruction (halt)
+
+  ASSERT_EQ(hvm.get_expression()->get<DataPair>().car()->get<Symbol>(),
+  Symbol("halt"));
+
+  // When: You invoke the evaluate_assembly_instruction() method again
+
+  auto result = hvm.evaluate_assembly_instruction();
+
+  // Then: You will get back the banana that is in the Accumulator
+
+  ASSERT_EQ(result->get<Symbol>(), Symbol("banana"));
+
+}
+
+/**
+ * @brief Test: evaluate_assembly_instruction() with (return) as exp
+ */
+TEST(HeapVirtualMachineUnitTest, evaluate_return) {
+
+  // Given: An Accumulator containing the symbol 'finale
+
+  Accumulator accumulator = create_node(Symbol("finale"));
+
+  // Given: An assembly instruction of the form (return)
+
+  Symbol ret("return");
+  Data return_data(ret);
+  DataPair return_pair(return_data);
+
+  NodePtr expression = create_node(return_pair);
+
+  // Given: An EnvPtr to an empty Environment
+
+  EnvPtr env = std::make_shared<Environment>(nullptr);
+
+  // Given: An empty ValueRib
+
+  ValueRib vr;
+
+  // Given: A CallFrame constructed as follows
+
+  EnvPtr call_frame_env = std::make_shared<Environment>(nullptr);
+  call_frame_env->set_value(Symbol("a"), create_node(String("Hello")));
+  call_frame_env->set_value(Symbol("b"), create_node(String("World")));
+
+  ValueRib call_frame_vr = {
+      create_node(String("VR1")),
+      create_node(String("VR2"))
+  };
+
+  Symbol halt("halt");
+  Data halt_data(halt);
+  DataPair halt_pair(halt_data);
+
+  NodePtr call_frame_exp = create_node(halt_pair);
+
+  FramePtr frame =
+      std::make_shared<CallFrame>(call_frame_exp, call_frame_env,
+                                  call_frame_vr, nullptr);
+
+  // Given: An instance of the HVM constructed with these items
+
+  HeapVirtualMachine hvm(accumulator, expression, env, vr, frame);
+
+  // When: You invoke the evaluate_assembly_instruction() method on hvm
+
+  hvm.evaluate_assembly_instruction();
+
+  // Then: The HVM's Environment register will have the bindings for a & b
+
+  ASSERT_EQ(hvm.get_environment()->get_value(Symbol("a"))->get<String>(),
+  String("Hello"));
+
+  ASSERT_EQ(hvm.get_environment()->get_value(Symbol("b"))->get<String>(),
+  String("World"));
+
+  // Then: The HVM's ValueRib register will contain "VR1" and "VR2"
+
+  ASSERT_EQ(hvm.get_value_rib()[0]->get<String>(), String("VR1"));
+
+  ASSERT_EQ(hvm.get_value_rib()[1]->get<String>(), String("VR2"));
+
+  // Then: The HVM's ControlStack (FramePtr) register will be nullptr
+
+  ASSERT_EQ(hvm.get_call_frame(), nullptr);
+
+  // Then: The HVM's Expression register will contain (halt)
+
+  ASSERT_EQ(hvm.get_expression()->get<DataPair>().car()->get<Symbol>(),
+  Symbol("halt"));
+
+  // When: You invoke the evaluate_assembly_instruction method again
+
+  auto result = hvm.evaluate_assembly_instruction();
+
+  // Then: The result you get back is the symbol 'finale
+
+  ASSERT_EQ(result->get<Symbol>(), Symbol("finale"));
 
 }
