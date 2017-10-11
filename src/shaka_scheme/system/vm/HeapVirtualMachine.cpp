d97342c6fd74ff4e4f14e766b220660b94f31206
Billy Troy Wooton <gcowootont@gmail.com>
2017-10-10 15:57:54 -1000

    Partial implementation of HeapVirtualMachine (#29)
    * Created HeapVirtualMachine.hpp
    
    * Made member variable names less terse
    
    * Created HeapVirtualMachine.cpp
    
    * Made methods for VM public access
    
    * Added include for Environment and started on eval
    
    * Added subdirectory for vm unit-tests to CMakeLists
    
    * Wrote tests for (halt) and (refer var x) instr.
    
    * Added functionality to evaluate_assembly
    
    * Wrote tests for const, and test
    
    * Added HeapVirtualMachine.cpp to SOURCE_FILES
    
    * Implemented assign, frame, argument, and return
    
    * Wrote tests for assign, frame, argument, & return
    
    * Fixed implementation of evaluate method
    
    * Updated comments in test to reflect changes

---
+++ src/shaka_scheme/system/vm/HeapVirtualMachine.cpp
@@ -0,0 +1,170 @@
+//
+// Created by Billy Wooton on 10/2/17.
+//
+
+#include "shaka_scheme/system/vm/HeapVirtualMachine.hpp"
+#include "shaka_scheme/system/base/Environment.hpp"
+#include "shaka_scheme/system/vm/CallFrame.hpp"
+
+namespace shaka {
+
+HeapVirtualMachine::~HeapVirtualMachine() {}
+
+void HeapVirtualMachine::evaluate_assembly_instruction() {
+  shaka::DataPair& exp_pair = exp->get<DataPair>();
+  shaka::Symbol& instruction = exp_pair.car()->get<Symbol>();
+
+  // (halt)
+  if (instruction == shaka::Symbol("halt")) {
+    return;
+  }
+
+  // (refer var x)
+  if (instruction == shaka::Symbol("refer")) {
+    shaka::DataPair& exp_cdr = exp_pair.cdr()->get<DataPair>();
+    shaka::Symbol& var = exp_cdr.car()->get<Symbol>();
+    this->set_accumulator(env->get_value(var));
+
+    DataPair& next_pair= exp_cdr.cdr()->get<DataPair>();
+    NodePtr next_expression = next_pair.car();
+
+    this->set_expression(next_expression);
+  }
+
+  // (constant obj x)
+  if (instruction == shaka::Symbol("constant")) {
+    shaka::DataPair& exp_cdr = exp_pair.cdr()->get<DataPair>();
+    NodePtr obj = exp_cdr.car();
+
+    this->set_accumulator(obj);
+
+    DataPair& next_pair = exp_cdr.cdr()->get<DataPair>();
+
+    NodePtr next_expression = next_pair.car();
+
+    this->set_expression(next_expression);
+  }
+
+  // (test then else)
+  if (instruction == shaka::Symbol("test")) {
+    shaka::DataPair& exp_cdr = exp_pair.cdr()->get<DataPair>();
+    NodePtr then_exp = exp_cdr.car();
+    NodePtr else_exp = exp_cdr.cdr()->get<DataPair>().car();
+
+    if (this->acc->get_type() == shaka::Data::Type::SYMBOL &&
+        this->acc->get<Symbol>() == Symbol("#f")) {
+
+      this->set_expression(else_exp);
+    }
+
+    else {
+      this->set_expression(then_exp);
+    }
+
+  }
+
+  // (assign var x)
+
+  if (instruction == shaka::Symbol("assign")) {
+    shaka::DataPair& exp_cdr = exp_pair.cdr()->get<DataPair>();
+    shaka::Symbol& var = exp_cdr.car()->get<Symbol>();
+
+    DataPair& next_pair = exp_cdr.cdr()->get<DataPair>();
+
+    NodePtr expression = next_pair.car();
+
+    this->env->set_value(var, this->acc);
+
+    this->set_expression(expression);
+
+  }
+
+  // (frame x ret)
+
+  if (instruction == shaka::Symbol("frame")) {
+    shaka::DataPair& expr_cdr = exp_pair.cdr()->get<DataPair>();
+
+    NodePtr x = expr_cdr.car();
+
+    NodePtr ret = expr_cdr.cdr()->get<DataPair>().car();
+
+    FramePtr new_frame =
+        std::make_shared<CallFrame>(ret, this->env,
+                                    this->rib, this->frame);
+    this->frame = new_frame;
+    this->set_expression(x);
+
+    shaka::ValueRib vr;
+
+    this->set_value_rib(vr);
+
+  }
+
+  // (argument x)
+
+  if (instruction == shaka::Symbol("argument")) {
+    shaka::DataPair& exp_cdr = exp_pair.cdr()->get<DataPair>();
+
+    NodePtr x = exp_cdr.car();
+
+    this->rib.push_back(this->acc);
+
+    this->set_expression(x);
+
+  }
+
+  // (return)
+
+  if (instruction == shaka::Symbol("return")) {
+    this->set_expression(this->frame->get_next_expression());
+    this->set_value_rib(this->frame->get_value_rib());
+    this->set_environment(this->frame->get_environment_pointer());
+    this->frame = this->frame->get_next_frame();
+
+  }
+
+}
+
+Accumulator HeapVirtualMachine::get_accumulator() const {
+  return this->acc;
+}
+
+Expression HeapVirtualMachine::get_expression() const {
+  return this->exp;
+}
+
+EnvPtr HeapVirtualMachine::get_environment() const {
+  return this->env;
+}
+
+ValueRib HeapVirtualMachine::get_value_rib() const {
+  return this->rib;
+}
+
+FramePtr HeapVirtualMachine::get_call_frame() const {
+  return this->frame;
+}
+
+void HeapVirtualMachine::set_accumulator(Accumulator a) {
+  this->acc = a;
+}
+
+void HeapVirtualMachine::set_expression(Expression x) {
+  this->exp = x;
+}
+
+void HeapVirtualMachine::push_call_frame(FramePtr s) {
+  this->frame = s;
+}
+
+void HeapVirtualMachine::set_environment(EnvPtr e) {
+  this->env = e;
+}
+
+void HeapVirtualMachine::set_value_rib(ValueRib r) {
+  this->rib = r;
+}
+
+
+} //namespace shaka
+
