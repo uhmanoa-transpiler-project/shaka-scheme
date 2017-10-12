dbfaf40eb5b9f44247a2684d8b8a8ec77a8f80e1
VermillionAzure <VermillionAzure@users.noreply.github.com>
2017-10-11 18:07:34 -1000

    Partial implementation of core type predicates (#33)
    * Finished partial impl. of non-list type predicates
    
    - Only for:
      - (eqv?)
      - (string?)
      - (symbol?)
      - (boolean?)
    
    * Added unit tests for partial impl of types.hpp

---
+++ tst/shaka_scheme/system/core/unit-core-types.cpp
@@ -0,0 +1,100 @@
+//
+// Created by aytas on 8/6/2017.
+//
+#include <gmock/gmock.h>
+#include <shaka_scheme/system/core/types.hpp>
+
+
+
+/**
+ * @brief Test: Testing (boolean?)
+ */
+TEST(TypesUnitTest, is_boolean) {
+  using namespace shaka;
+
+  // Given: a boolean node and a null list
+  NodePtr node = create_node(Data(Boolean(false)));
+  NodePtr null_list_node = create_node(Data());
+
+  // When: you apply the predicate is_boolean()
+
+  // Then: it should be true for a boolean
+  ASSERT_TRUE(core::is_boolean(node));
+  // Then: it should be false for a non-boolean
+  ASSERT_FALSE(core::is_boolean(null_list_node));
+}
+
+/**
+ * @brief Test: Testing (string?)
+ */
+TEST(TypesUnitTest, is_string) {
+  using namespace shaka;
+
+  // Given: a string node and a null list
+  NodePtr node = create_node(Data(String("hi")));
+  NodePtr null_list_node = create_node(Data());
+
+  // When: you apply the predicate is_string()
+
+  // Then: it should be true for a string
+  ASSERT_TRUE(core::is_string(node));
+  // Then: it should be false for a non-string
+  ASSERT_FALSE(core::is_string(null_list_node));
+}
+
+/**
+ * @brief Test: Testing (symbol?)
+ */
+TEST(TypesUnitTest, is_symbol) {
+  using namespace shaka;
+
+  // Given: a symbol node and a null list
+  NodePtr node = create_node(Data(Symbol("hi")));
+  NodePtr null_list_node = create_node(Data());
+
+  // When: you apply the predicate is_symbol()
+
+  // Then: it should be true for a symbol
+  ASSERT_TRUE(core::is_symbol(node));
+  // Then: it should be false for a non-symbol
+  ASSERT_FALSE(core::is_symbol(null_list_node));
+}
+
+/**
+ * @brief Test: Testing unspecified(), a special implementation-specific type
+ * predicate for detecting "unspecified" values.
+ */
+TEST(TypesUnitTest, is_unspecified) {
+  using namespace shaka;
+
+  // Given: an unspecified value node and a null list
+  NodePtr node = core::create_unspecified_node();
+  NodePtr null_list_node = create_node(Data());
+
+  // When: you apply the predicate is_unspecified()
+
+  // Then: it should be true for an unspecified value
+  ASSERT_TRUE(core::is_unspecified(node));
+  // Then: it should be false for a non-unspecified value
+  ASSERT_FALSE(core::is_unspecified(null_list_node));
+}
+
+/**
+ * @brief Test: Testing (eqv?)
+ */
+TEST(TypesUnitTest, is_eqv) {
+  using namespace shaka;
+
+  // Given: two distinct nodes with the same string value.
+  NodePtr node1 = create_node(Data(String("hi")));
+  NodePtr node2 = create_node(Data(String("hi")));
+
+  // When: you apply the equivalence
+
+  // Then: it should be true for nodes with itself.
+  ASSERT_TRUE(core::is_eqv(node1, node1));
+  ASSERT_TRUE(core::is_eqv(node2, node2));
+  // Then: it should be false for nodes not to themselves.
+  ASSERT_FALSE(core::is_eqv(node1, node2));
+  ASSERT_FALSE(core::is_eqv(node2, node1));
+}
