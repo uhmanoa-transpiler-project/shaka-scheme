70b2e4049fde97b95885e78d33952cab2dfb5fac
Billy Troy Wooton <gcowootont@gmail.com>
2017-10-12 11:03:21 -1000

    Made string-append function variable arity

--- src/shaka_scheme/system/vm/strings.hpp
+++ src/shaka_scheme/system/vm/strings.hpp
@@ -15,18 +15,23 @@ namespace shaka {
 
 using Callable = std::function<std::vector<NodePtr>(std::vector<NodePtr>)>;
 
-std::vector<NodePtr> str_length(std::vector<NodePtr> args) {
+std::vector<NodePtr> str_append(std::vector<NodePtr> args) {
   if (args[0]->get_type() != Data::Type::STRING) {
     throw TypeException(10001, "Incompatible argument type to NativeClosure");
   }
 
-  std::vector<NodePtr> results;
+  String result(args[0]->get<String>());
 
-  results[0] = args[0]->get<String>()->length();
+
+  for (std::size_t i = 1; i < args.size(); i++) {
+    result.append(args[i]->get<String>());
+  }
+
+  std::vector<NodePtr> results = {create_node(result)};
   return results;
 }
 
-Callable string_length = str_length;
+Callable string_append = str_append;
 
 }
 #endif //SHAKA_SCHEME_STRINGS_HPP
