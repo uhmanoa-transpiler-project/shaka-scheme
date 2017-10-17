3c96f1038564d0a1fe1f932204f8ee959caa5698
Austin Tasato <aytasato@gmail.com>
2017-10-17 01:15:09 -1000

    Changed dummy main.cpp into lexer REPL

--- main.cpp
+++ main.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
 
 #include "shaka_scheme/system/exceptions/BaseException.hpp"
+#include "shaka_scheme/system/lexer/Tokenizer.hpp"
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+  std::cout << "Welcome to Shaka Scheme!" << std::endl;
+
+  bool done = false;
+  shaka::Token token(shaka::Token::Type::END_OF_FILE, "\0");
+  shaka::Tokenizer input(std::cin);
+  while (!done) {
+    std::cout << "> " << std::flush;
+    try {
+      token = input.get();
+      std::cout << token << std::endl;
+      if (token.get_type() == shaka::Token::Type::DIRECTIVE) {
+        if (token.get_string() == std::string("quit")) {
+          done = true;
+        }
+      }
+    } catch (shaka::BaseException e) {
+      std::cerr << e.what() << std::endl;
+      std::cin.clear();
+      std::cin.ignore(INT_MAX);
+    }
+  }
+
+  std::cout << "Exiting..." << std::endl;
   return 0;
 }
