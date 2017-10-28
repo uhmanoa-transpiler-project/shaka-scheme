c2c3692ee364f5aa318e025027353b724b278002
Billy Troy Wooton <gcowootont@gmail.com>
2017-10-27 18:07:55 -1000

    Fixed implementation of string-append

--- src/shaka_scheme/system/vm/strings.hpp
+++ src/shaka_scheme/system/vm/strings.hpp
@@ -15,15 +15,16 @@ namespace shaka {
 
 using Callable = std::function<std::vector<NodePtr>(std::vector<NodePtr>)>;
 
+namespace core {
+
 std::vector<NodePtr> str_append(std::vector<NodePtr> args) {
   if (args[0]->get_type() != Data::Type::STRING) {
     throw TypeException(10001, "Incompatible argument type to NativeClosure");
   }
 
-  String result(args[0]->get<String>());
+  String result("");
 
-
-  for (std::size_t i = 1; i < args.size(); i++) {
+  for (int i = args.size() - 1; i >= 0; i--) {
     result.append(args[i]->get<String>());
   }
 
@@ -31,7 +32,9 @@ std::vector<NodePtr> str_append(std::vector<NodePtr> args) {
   return results;
 }
 
-Callable string_append = str_append;
+}
+
+Callable string_append = core::str_append;
 
 }
 #endif //SHAKA_SCHEME_STRINGS_HPP
