0611fd02cdf8192967b803458fc64f75a3da5bb8
1010jms <1010jms@gmail.com>
2017-11-29 15:39:15 -1000

    Updated params of funcs in strings.hpp and symbols.hpp & started (string-length)

--- src/shaka_scheme/system/core/symbols.hpp
+++ src/shaka_scheme/system/core/symbols.hpp
@@ -14,16 +14,16 @@ namespace core {
 
 /**
  * @brief Implements (symbol=?).
- * @param args The data to take in
+ * @param nodes The data to take in
  * @return A boolean of whether the predicate was satisfied.
  */
-inline bool are_symbols(std::deque<NodePtr> args){
-    for(std::size_t i = 0; i < args.size(); i++) {
-        if (args[i]->get_type() != Data::Type::SYMBOL)
+inline bool are_symbols(std::deque<NodePtr> nodes){
+    for(std::size_t i = 0; i < nodes.size(); i++) {
+        if (nodes[i]->get_type() != Data::Type::SYMBOL)
             return false;
 
-        if(args[i+1] != nullptr)
-            if(args[i] !=  args[i+1])
+        if(nodes[i+1] != nullptr)
+            if(nodes[i] !=  nodes[i+1])
                 return false;
     }
     return true;
@@ -31,15 +31,15 @@ inline bool are_symbols(std::deque<NodePtr> args){
 
 /**
  * @brief Implements (string->symbol)
- * @param arg The data to take in
+ * @param node The data to take in
  * @return A NodePtr containing the symbol
  */
-inline NodePtr string_to_symbol(NodePtr arg){
-    if (arg->get_type() != Data::Type::STRING) {
+inline NodePtr string_to_symbol(NodePtr node){
+    if (node->get_type() != Data::Type::STRING) {
         throw TypeException(10001, "Incompatible argument type to NativeClosure");
     }
 
-    String str(arg->get<String>());
+    String str(node->get<String>());
 
     NodePtr results = create_node(Data(Symbol(str.get_string())));
     return results;
@@ -47,15 +47,15 @@ inline NodePtr string_to_symbol(NodePtr arg){
 
 /**
  * @brief Implements (symbol->string)
- * @param arg The data to take in
+ * @param node The data to take in
  * @return A NodePtr containing the string
  */
-inline NodePtr symbol_to_string(NodePtr arg) {
-    if (arg->get_type() != Data::Type::SYMBOL) {
+inline NodePtr symbol_to_string(NodePtr node) {
+    if (node->get_type() != Data::Type::SYMBOL) {
         throw TypeException(10001, "Incompatible argument type to NativeClosure");
     }
 
-    Symbol sym(arg->get<Symbol>());
+    Symbol sym(node->get<Symbol>());
 
     NodePtr results = create_node(Data(String(sym.get_value())));
     return results;
