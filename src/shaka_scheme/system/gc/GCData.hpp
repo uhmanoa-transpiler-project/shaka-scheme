bdc8b44d9ead4e09c1f42f813cad05f5231d5125
Billy Troy Wooton <gcowootont@gmail.com>
2018-02-08 21:07:42 -1000

    Fixed add library directive

---
+++ src/shaka_scheme/system/gc/GCData.hpp
@@ -0,0 +1,8 @@
+//
+// Created by mortrax on 2/8/18.
+//
+
+#ifndef SHAKA_SCHEME_GCDATA_HPP
+#define SHAKA_SCHEME_GCDATA_HPP
+
+#endif //SHAKA_SCHEME_GCDATA_HPP
---
+++ src/shaka_scheme/system/gc/GCList.hpp
@@ -0,0 +1,8 @@
+//
+// Created by mortrax on 2/8/18.
+//
+
+#ifndef SHAKA_SCHEME_GCLIST_HPP
+#define SHAKA_SCHEME_GCLIST_HPP
+
+#endif //SHAKA_SCHEME_GCLIST_HPP
