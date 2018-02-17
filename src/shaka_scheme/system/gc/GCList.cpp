2b5908fd1bc722e4aa70277e5d6bff73b0ebd2e9
Brandon Wong <bwong7@hawaii.edu>
2018-02-17 10:59:27 -1000

    Created cpp file for GCList

---
+++ src/shaka_scheme/system/gc/GCList.cpp
@@ -0,0 +1,21 @@
+//
+// Created by Brandon Wong on 2/17/18.
+//
+
+#include "shaka_scheme/system/gc/GCData.hpp"
+#include "shaka_scheme/system/gc/GCList.hpp"
+
+namespace shaka {
+
+    GCList::GCList() {}
+
+    GCList::~GCList() {}
+
+    void GCList::add_data(GCData *data) {
+
+    }
+
+    void GCList::sweep() {
+
+    }
+}
