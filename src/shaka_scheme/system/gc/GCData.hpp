c7e81ec4f02f9e85f56b91982e18f0bfc92d65f8
Billy Troy Wooton <gcowootont@gmail.com>
2018-02-23 18:21:29 -1000

    Implemented GCData (#55)
    * Added GCData.cpp
    
    * Added set_next(), get_data(), get_data_address()
    
    * Implemented GCData
    
    * Add subdirectory gc
    
    * Create CMakeLists.txt for gc
    
    * Unit tests for GCData

--- src/shaka_scheme/system/gc/GCData.hpp
+++ src/shaka_scheme/system/gc/GCData.hpp
@@ -5,6 +5,8 @@
 #ifndef SHAKA_SCHEME_GCDATA_HPP
 #define SHAKA_SCHEME_GCDATA_HPP
 
+#include "shaka_scheme/system/base/Data.hpp"
+
 namespace shaka {
 
     /**
@@ -18,9 +20,12 @@ namespace shaka {
         ~GCData();
 
         GCData *get_next();
+        void set_next(GCData *next_node);
         void mark();
         void unmark();
         bool is_marked();
+        Data& get_data();
+        Data *get_data_address();
 
     private:
         bool marked;
