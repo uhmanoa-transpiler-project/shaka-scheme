ed8f8e37ed41bde78e7416227a33435d38425d95
Brandon Wong <bwong7@hawaii.edu>
2018-02-23 18:54:52 -1000

    Constructed unit tests for GCList methods

--- tst/shaka_scheme/system/gc/unit-GCList.cpp
+++ tst/shaka_scheme/system/gc/unit-GCList.cpp
@@ -5,8 +5,60 @@
 #include <gmock/gmock.h>
 #include "shaka_scheme/system/gc/GCList.hpp"
 
+/**
+ * @Test: Construction of GCList
+ */
 TEST(GCListUnitTest, constructor_test) {
+
+    // Given: A constructed GCList
+
     shaka::GCList list;
+
+    // Then: It is empty
     
     ASSERT_TRUE(list.is_empty());
+}
+
+/**
+ * @Test: Destruction of empty GCList
+ */
+TEST(GCListUnitTest, destructor_test_empty) {
+
+    // Given: A constructed GCList
+
+    shaka::GCList list;
+
+    // Then: You can invoke destructor
+
+    list.~GCList();
+}
+
+/**
+ * @Test: Add GCData to GCList
+ */
+TEST(GCListUnitTest, add_GCData) {
+
+    // Given: A constructed GCList
+
+    // When: You add 3 GCData to GCList
+
+    // Then: Size of GCList should be 3
+}
+
+/**
+ * @Test: Destruction of nonempty GCList
+ */
+TEST(GCListUnitTest, destructor_test_nonempty) {
+
+    // Given: A constructed GCList
+
+}
+
+/**
+ * @Test: Sweep GCList
+ */
+TEST(GCListUnitTest, sweep_GCList) {
+
+    // Given: A constructed GCList
+
 }
