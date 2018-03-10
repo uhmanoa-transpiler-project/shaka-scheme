ad45a42c6eff951dec97755e484e24147770a6f0
JMS1010 <1010jms@gmail.com>
2018-03-09 15:39:01 -1000

    Edited constructors & added comments to operators
    * Added a default constructor
    * Moved function bodies of copy & str constructor to cpp file
    * Moved function body of 'get_value()' to cpp file
    * Added comments for each constructor
    * Added comments for each arithmetic operator
    * Added comments for each comparison operator
    * Removed cpp and unit-test file

--- src/shaka_scheme/system/base/BigInteger.hpp
+++ src/shaka_scheme/system/base/BigInteger.hpp
@@ -5,18 +5,42 @@
 #ifndef SHAKA_SCHEME_BIGINTEGER_HPP
 #define SHAKA_SCHEME_BIGINTEGER_HPP
 
+#include <string>
+
 namespace shaka {
 
+    /**
+     * @brief Imports the representation of Rational to be used by BigInteger
+     */
     class Rational;
+
+    /**
+     * @brief Imports the representation of Real to be used by BigInteger
+     */
     class Real;
 
+    /**
+     * @brief Representation of integer values larger than 64-bits
+     */
     class BigInteger {
     public:
 
-        BigInteger(const BigInteger& other) :
-                value(other.value) {};
-        BigInteger(std::string val) :
-                value(val) {};
+        /**
+         * @brief Default Constructor
+         */
+        BigInteger();
+
+        /**
+         * @brief Copy constructor
+         * @param other the BigInteger that is being copied
+         */
+        BigInteger(const BigInteger& other);
+
+        /**
+         * @brief Constructor whose value is initialized to a given string
+         * @param val input string
+         */
+        BigInteger(std::string val);
 
         /**
          * @brief Implicit type conversion operator for BigInteger -> Rational
@@ -24,6 +48,7 @@ namespace shaka {
          * @return The Rational conversion for this BigInteger object
          */
         operator Rational();
+
         /**
          * @brief Implicit type conversion operator for BigInteger -> Real
          * Needed for interop between numeric subtypes in operator overloads
@@ -31,25 +56,104 @@ namespace shaka {
          */
         operator Real();
 
-        // arithmetic operators for BigIntegers
+        /**
+         * @brief Addition operator for BigIntegers
+         * @param lhs The first BigInteger operand
+         * @param rhs The second BigInteger operand
+         * @return The sum of lhs and rhs
+         */
         friend BigInteger operator+(const BigInteger& lhs, const BigInteger& rhs);
+
+        /**
+         * @brief Subtraction operator for BigIntegers
+         * @param lhs The first BigInteger operand
+         * @param rhs The second BigInteger operand
+         * @return The difference of lhs and rhs
+         */
         friend BigInteger operator-(const BigInteger& lhs, const BigInteger& rhs);
+
+        /**
+         * @brief Multiplication operator for BigIntegers
+         * @param lhs The first BigInteger operand
+         * @param rhs The second BigInteger operand
+         * @return The product of lhs and rhs
+         */
         friend BigInteger operator*(const BigInteger& lhs, const BigInteger& rhs);
+
+        /**
+         * @brief Fraction operator for BigIntegers
+         * @param lhs The first BigInteger operand
+         * @param rhs The second BigInteger operand
+         * @return A Rational value with lhs as the numerator
+           and rhs as the denominator
+         */
         friend Rational operator/(const BigInteger& lhs, const BigInteger& rhs);
+
+        /**
+         * @brief Division operator for BigIntegers
+         * @param lhs The first BigInteger operand
+         * @param rhs The second BigInteger operand
+         * @return The quotient of lhs and rhs
+         */
         friend BigInteger operator|(const BigInteger& lhs, const BigInteger& rhs);
+
+        /**
+         * @brief Modulo operator for BigIntegers
+         * @param lhs The first BigInteger operand
+         * @param rhs The second BigInteger operand
+         * @return The remainder of the quotient of lhs and rhs
+         */
         friend BigInteger operator%(const BigInteger& lhs, const BigInteger& rhs);
 
-        // comparison operators for BigIntegers
+        /**
+         * @brief Equality operator for BigIntegers
+         * @param lhs The first BigInteger operand
+         * @param rhs The second BigInteger operand
+         * @return Returns 'true' if lhs and rhs are equal
+         */
         friend bool operator==(const BigInteger& lhs, const BigInteger& rhs);
+
+        /**
+         * @brief Greater-Than operator for BigIntegers
+         * @param lhs The first BigInteger operand
+         * @param rhs The second BigInteger operand
+         * @return Returns 'true' if lhs is greater than rhs
+         */
         friend bool operator>(const BigInteger& lhs, const BigInteger& rhs);
+
+        /**
+         * @brief Less-Than operator for BigIntegers
+         * @param lhs The first BigInteger operand
+         * @param rhs The second BigInteger operand
+         * @return Returns 'true' if lhs is less than rhs
+         */
         friend bool operator<(const BigInteger& lhs, const BigInteger& rhs);
+
+        /**
+         * @brief Greater-Than-Or-Equal operator for BigIntegers
+         * @param lhs The first BigInteger operand
+         * @param rhs The second BigInteger operand
+         * @return Returns 'true' if lhs is greater than or equal to rhs
+         */
         friend bool operator>=(const BigInteger& lhs, const BigInteger& rhs);
+
+        /**
+         * @brief Less-Than-Or-Equal operator for BigIntegers
+         * @param lhs The first BigInteger operand
+         * @param rhs The second BigInteger operand
+         * @return Returns 'true' if lhs is less than or equal to rhs
+         */
         friend bool operator<=(const BigInteger& lhs, const BigInteger& rhs);
+
+        /**
+         * @brief Inequality operator for BigIntegers
+         * @param lhs The first BigInteger operand
+         * @param rhs The second BigInteger operand
+         * @return Returns 'true' if lhs and rhs are not equal
+         */
         friend bool operator!=(const BigInteger& lhs, const BigInteger& rhs);
 
-        std::string get_value() const {
-            return this->value;
-        }
+        std::string get_value() const;
 
     private:
         std::string value;
