f821416a306a4800dba521973ac5605439bafd2e
Kirsten0604 <hungrytacocat@gmail.com>
2018-03-18 17:22:10 -1000

    unit tests

--- tst/shaka_scheme/system/gc/unit-GCNode.cpp
+++ tst/shaka_scheme/system/gc/unit-GCNode.cpp
@@ -1,10 +1,10 @@
+//(*gcn).get<shaka::Number>()
 //
 // Created by Kirsten on 2/25/2018.
 //
 #include <gmock/gmock.h>
 #include "shaka_scheme/system/gc/GCData.hpp"
 #include "shaka_scheme/system/gc/GCNode.hpp"
-
 
 /**
  * @Test: Test constructor of GCNode
@@ -21,8 +21,14 @@ TEST (GCNodeUnitTest, test_constructor) {
 }
 
 /**
- * Test: Test dreference operator
+ * @Test: Test dreference operator of GCNode
  */
 TEST (GCNodeUnitTest, test_) {
 
+    //Given: You construct a GCNode as follows
+    shaka::GCData* gcd = new shaka::GCData(shaka::Data(shaka::Number(5)));
+    shaka::GCNode gcn(gcd);
+    
+    //Then: the data in gcn is equal to 5
+    ASSERT_EQ((*gcn).get<shaka::Number>(), shaka::Number(5));
 }
