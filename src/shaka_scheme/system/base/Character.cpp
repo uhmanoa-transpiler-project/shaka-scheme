a94e6c1c1856f12ed142b33281772e7922422d29
lihongman <herman_hmli@yahoo.com>
2018-03-23 12:50:03 -1000

    Character class and tests implemented
    * Tests only cover the character comparison operators
    * Using switch statement currently for hardcoded outputs
    * Ex. #/null #/return etc.

--- src/shaka_scheme/system/base/Character.cpp
+++ src/shaka_scheme/system/base/Character.cpp
@@ -2,3 +2,78 @@
 // Created by Herman on 3/7/2018.
 //
 
+#include "Character.hpp"
+#include <string>
+
+namespace shaka{
+
+Character::Character(int val) {
+  if(val > -1 && val < 128) {
+    this->value = (char)val;
+    return;
+  }
+  this->value = '\0';
+}
+
+bool operator==(const Character& a, const Character& b) {
+  return a.value == b.value;
+}
+
+bool operator>(const Character& a, const Character& b) {
+  return a.value > b.value;
+}
+
+bool operator<(const Character& a, const Character& b) {
+  return a.value < b.value;
+}
+
+bool operator>=(const Character& a, const Character& b) {
+  return a.value >= b.value;
+}
+
+bool operator<=(const Character& a, const Character& b) {
+  return a.value <= b.value;
+}
+
+bool operator!=(const Character& a, const Character& b) {
+  return a.value != b.value;
+}
+
+std::ostream& operator<<(std::ostream& out, const shaka::Character& chara) {
+  std::string output = "#\\";
+  switch((int)chara.value) {
+  case 0:
+    output += "null";
+    break;
+  case 7:
+    output += "alarm";
+    break;
+  case 8:
+    output += "backspace";
+    break;
+  case 9:
+    output += "tab";
+    break;
+  case 10:
+    output += "newline";
+    break;
+  case 13:
+    output += "return";
+    break;
+  case 27:
+    output += "escape";
+    break;
+  case 32:
+    output += "space";
+    break;
+  case 127:
+    output += "delete";
+    break;
+  default:
+    output += chara.value;
+  }
+  out << output;
+  return out;
+}
+
+} // namespace shaka
--- src/shaka_scheme/system/base/Character.hpp
+++ src/shaka_scheme/system/base/Character.hpp
@@ -5,13 +5,35 @@
 #ifndef SHAKA_SCHEME_CHARACTER_HPP
 #define SHAKA_SCHEME_CHARACTER_HPP
 
+#include <iostream>
+
 namespace shaka {
 
 class Character {
 public:
+  /**
+   * @brief Copy constructor for character.
+   * @param other Character object to be copied.
+   */
   Character(const Character& other) : value(other.value) {};
-  Character(int val) : value(val) {};
+
+  /**
+   * @brief Constructor created via an integer.
+   * @param val Integer to be casted to character.
+   */
+  Character(int val);
+
+  /**
+   * @brief Constructor created via a char.
+   * @param val Character to be added.
+   */
   Character(char val) : value(val) {};
+
+  /**
+   * @brief Empty constructor sets value at null character.
+   */
+  Character() : value('\0') {};
+
 
   friend bool operator==(const Character&, const Character&);
   friend bool operator>(const Character&, const Character&);
@@ -20,7 +42,9 @@ public:
   friend bool operator<=(const Character&, const Character&);
   friend bool operator!=(const Character&, const Character&);
 
-  const char get_value() { return value; }
+  friend std::ostream& operator<<(std::ostream&, const shaka::Character&);
+
+  char get_value() const { return value; }
 private:
   char value;
 };
