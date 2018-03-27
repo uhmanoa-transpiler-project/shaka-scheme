610ba1fe0fb96cd711c0d4f0bb81098bc656a7e7
Austin Tasato <aytasato@gmail.com>
2018-03-26 15:01:45 -1000

    Added parser support for bytevectors/vectors
    - Also added more comments to parse_list()

--- src/shaka_scheme/system/parser/parser_definitions.cpp
+++ src/shaka_scheme/system/parser/parser_definitions.cpp
@@ -98,12 +98,14 @@ using DataConstructor = std::function<NodePtr(ParserResult)>;
  */
 ParserResult parse_simple(ParserInput& in) {
   auto next = in.peek();
+  // Stop if we have no more input or there was a LexerError
   if (next.is_incomplete()) {
     return Incomplete(next);
   } else if (next.is_error()) {
     return LexerError(next);
   }
-  //std::cout << next << std::endl;
+  // Get the singleton, "simple" tokens and convert them into strings,
+  // or return a ParserError
   if (next.token_type == "string") {
     in.get();
     return Complete(create_node(String(next.str)));
@@ -121,6 +123,9 @@ ParserResult parse_simple(ParserInput& in) {
     return Complete(create_node(Number(Integer(std::stoi(next.str)))));
   } else if (next.token_type == "rational") {
     in.get();
+    // When converting the string form of a rational, we need to find the
+    // position of the "/" in the string, and then split into the numerator
+    // and denominator there.
     const auto slash_it = next.str.find("/");
     return Complete(
         create_node(
@@ -138,29 +143,37 @@ ParserResult parse_simple(ParserInput& in) {
 }
 
 ParserResult parse_list(ParserInput& in) {
-  //std::cout << "parse-list" << std::endl;
+  // Create our accumulator list that we will append the parsed elements onto
   auto data_list = core::list();
-  //std::cout << "TOKEN: " << in.peek() << std::endl;
+
+  // We will want to possibly save our tokens in the future
+  /// @todo Make sure that tokens is used to correctly place the consumed
+  /// tokens back onto the input if the input list is incomplete
   std::vector<lexer::LexResult> tokens;
+
+  // Read the (
   if (in.peek().token_type == "paren-left") {
     tokens.emplace_back(in.get());
   }
 
+  // Keep reading elements (datums) while we have not reached the )
   while (in.peek().token_type != "paren-right") {
-    //std::cout << "TOKEN: " << in.peek() << std::endl;
     if (in.peek().is_incomplete()) {
       auto incomp = in.peek();
       auto reversed_list = core::reverse(data_list);
     }
+    // Read in a datum
     ParserResult datum_result = parse_datum(in);
-    //std::cout << "DATUM:" << datum_result << std::endl;
+    // If it was complete, add it onto our list
     if (datum_result.is_complete()) {
       data_list = core::append(data_list, core::list(datum_result.it));
     } else if (in.peek().token_type == "dot") {
+      // However, if we have an improper list, we must end shortly after this.
       in.get();
-      //std::cout << "in.peek(): " << in.peek() << std::endl;
+      // Read in the last datum in the improper list
       datum_result = parse_datum(in);
-      //std::cout << "IMPROPER LIST LAST DATUM: " << datum_result << std::endl;
+      // If the last datum is correct, we must append it as the last item
+      // onto the end of the list
       if (datum_result.is_complete()) {
         data_list = core::append(data_list, datum_result.it);
       } else {
@@ -168,20 +181,104 @@ ParserResult parse_list(ParserInput& in) {
             "last datum for improper list");
       }
     } else {
+      // Otherwise, we have an incomplete parse, and let's return the
+      // Incomplete status which is in the datum result.
       return datum_result;
     }
   }
+  // We must match to the last paren
   if (in.peek().token_type == "paren-right") {
-    //std::cout << "RPAREN: " << in.peek() << std::endl;
     in.get();
-    //std::cout << "END OF LIST: " << in.peek() << std::endl;
     return Complete(data_list);
   } else {
+    // Otherwise, we have a parse error.
     return ParserError(in.peek(),
                        "could not match to closing parens for list");
   }
 }
 
+ParserResult parse_vector(ParserInput& in) {
+  auto data_list = core::list();
+  std::vector<lexer::LexResult> tokens;
+  if (in.peek().token_type == "vector-left") {
+    tokens.emplace_back(in.get());
+  }
+
+  while (in.peek().token_type != "paren-right") {
+    if (in.peek().is_incomplete()) {
+      auto incomp = in.peek();
+      auto reversed_list = core::reverse(data_list);
+    }
+    ParserResult datum_result = parse_datum(in);
+    if (datum_result.is_complete()) {
+      data_list = core::append(data_list, core::list(datum_result.it));
+    } else {
+      return datum_result;
+    }
+  }
+  if (in.peek().token_type == "paren-right") {
+    in.get();
+    // This is the same as parse_list, but we are instead just converting it
+    // into a vector
+    shaka::NodePtr converted_vector = core::list_to_vector(data_list);
+    return Complete(converted_vector);
+  } else {
+    return ParserError(in.peek(),
+                       "could not match to closing parens for vector");
+  }
+}
+
+ParserResult parse_bytevector(ParserInput& in) {
+  // Let us use a vector to hold the elements to have better memory access
+  // patterns
+  std::vector<unsigned char> elements;
+  std::vector<lexer::LexResult> tokens;
+  if (in.peek().token_type == "bytevector-left") {
+    tokens.emplace_back(in.get());
+  }
+
+  while (in.peek().token_type != "paren-right") {
+    if (in.peek().is_incomplete()) {
+      auto incomp = in.peek();
+    }
+    ParserResult datum_result = parse_datum(in);
+    if (datum_result.is_complete()) {
+      auto current_element = *datum_result.it;
+      if (current_element.get_type() != shaka::Data::Type::NUMBER) {
+        return ParserError(in.peek(),
+                           "bytevectors cannot contain non-numbers");
+      }
+      const auto current_number = current_element.get<shaka::Number>();
+      if (current_number.get_type() !=
+          shaka::Number::NumberType::INTEGER) {
+        return ParserError(in.peek(),
+                           "bytevectors cannot contain numbers that are not "
+                               "integers");
+      }
+      const auto current_value = current_number.get<shaka::Integer>();
+      if (current_value.get_value() < 0 || current_value.get_value() > 255) {
+        return ParserError(in.peek(),
+                           "bytevectors can only contain unsigned integers "
+                               "within the range 0-255"
+        );
+      }
+      elements.push_back(static_cast<unsigned char>(current_value.get_value()));
+    } else {
+      return datum_result;
+    }
+  }
+  if (in.peek().token_type == "paren-right") {
+    in.get();
+    shaka::Bytevector bv(elements.size());
+    for (std::size_t i = 0; i < elements.size(); ++i) {
+      bv[i] = std::move(elements[i]);
+    }
+    return Complete(create_node(shaka::Data(std::move(bv))));
+  } else {
+    return ParserError(in.peek(),
+                       "could not match to closing parens for bytevector");
+  }
+}
 
 ParserResult parse_datum(ParserInput& in) {
   while (in.peek().token_type == "comment") {
@@ -219,6 +316,10 @@ ParserResult parse_datum(ParserInput& in) {
     auto next = in.peek();
     if (next.token_type == "paren-left") {
       return parse_list(in);
+    } else if (next.token_type == "vector-left") {
+      return parse_vector(in);
+    } else if (next.token_type == "bytevector-left") {
+      return parse_bytevector(in);
     } else {
       return ParserError(next, "could not parse datum");
     }
