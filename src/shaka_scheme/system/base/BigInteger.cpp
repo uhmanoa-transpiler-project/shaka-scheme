bf1411f2c67ee8b14081fded669414a2ad0f9866
Jared Shimabukuro <jshima@hawaii.edu>
2018-04-18 16:54:32 -1000

    Implemented vector value in BigInteger (#73)
    * Implemented Initial Implementation & Test
    
    - Created BigInteger.cpp which contains comments
    for implementing each function
    - Created unit-BigInteger.cpp with constructor
    test case
    - Added BigInteger.cpp & unit-BigInteger.cpp to their
    respective CMakeLists.txt files
    
    * Edited constructors & added comments to operators
    
    * Added a default constructor
    * Moved function bodies of copy & str constructor to cpp file
    * Moved function body of 'get_value()' to cpp file
    * Added comments for each constructor
    * Added comments for each arithmetic operator
    * Added comments for each comparison operator
    * Removed cpp and unit-test file
    
    * Added initial implementation and test files
    
    * Moved vector functionality to main BigInt files
    
    - Deleted "jshima" files
    - Moved vector definition to BigInteger.hpp
    - Moved vector getter definition to BigInteger.hpp
    - Moved vector constructor to BigInteger.cpp
    - Moved vector getter to BigInteger.cpp
    - Moved tests to unit-BigInteger.cpp
    
    * BigInt.cpp & unit tests now include correct files
    
    * Added comments to unit-BigInteger.cpp
    
    - Added comments to each test case following the
    "Given, When, Then" format
    - Slightly modified 'test_really_really_big_number' to
    more accurately test the values of the third BigInteger
    (n3)

---
+++ src/shaka_scheme/system/base/BigInteger.cpp
@@ -0,0 +1,206 @@
+//
+// Created by Jared Shimabukuro on 3/3/18.
+//
+
+#include "shaka_scheme/system/base/BigInteger.hpp"
+#include "shaka_scheme/system/base/Rational.hpp"
+#include "shaka_scheme/system/base/Real.hpp"
+
+namespace shaka {
+
+    BigInteger::BigInteger() :
+    str_value("0") {
+        this->value.push_back(0);
+    }
+
+    BigInteger::BigInteger(const BigInteger& other) :
+	value(other.value), str_value(other.str_value) {}
+
+    BigInteger::BigInteger(std::string val) :
+    str_value(val) {
+        for(std::int64_t i = (val.length()); i > 0; i-= 18) {
+            if(i < 18) {
+                if (val[0] == '-') {
+                    sign = true;
+                    this->value.insert(this->value.begin(), std::stoull(val.substr(1, (i-1))));
+                }
+
+                else {
+                    sign = false;
+                    this->value.insert(this->value.begin(), std::stoull(val.substr(0,i)));
+                }
+            }
+
+            else
+                this->value.insert(this->value.begin(), std::stoull(val.substr((i-18),18)));
+        }
+    }
+
+    BigInteger::BigInteger(bool s, std::vector<std::uint64_t> val) :
+    value(val) {
+        if (s) {
+            sign = true;
+            this->str_value = "-";
+        }
+        else {
+            sign = false;
+            this->str_value = "";
+        }
+
+
+        for(int i = 0; i < val.size(); i++) {
+            this->str_value.append(std::to_string(val[i]));
+        }
+    }
+
+    BigInteger::operator Rational() {
+        /*************************************************/
+        /*************************************************/
+        /*** FIGURE OUT HOW TO IMPLEMENT THIS FUNCTION ***/
+        /*************************************************/
+        /*************************************************/
+    }
+
+    /**
+     * @todo Test the conversion from BigInteger to Real
+     */
+    BigInteger::operator Real() {
+        double result;
+        return result;
+    }
+
+    BigInteger operator+(const BigInteger& lhs, const BigInteger& rhs) {
+        BigInteger result;
+
+        /**
+         * @todo Implement the Parallel Algorithm (Preferred):
+         * https://arxiv.org/ftp/arxiv/papers/1204/1204.0232.pdf
+         *
+         * Reference to C++ thread commands:
+         * http://en.cppreference.com/w/c/thread
+         */
+
+        /**
+         * @todo Or Implement the Sequential Algorithm (Backup Plan):
+         * https://arxiv.org/ftp/arxiv/papers/1204/1204.0232.pdf
+         */
+
+        /**
+         * @todo Or Implement the Math-by-Hand Method (Last Resort):
+         * https://silentmatt.com/blog/2011/10/how-bigintegers-work/
+         */
+
+        return result;
+    }
+
+    BigInteger operator-(const BigInteger& lhs, const BigInteger& rhs) {
+        BigInteger result;
+
+        /**
+         * @todo Implement the Parallel Algorithm (Preferred):
+         * https://arxiv.org/ftp/arxiv/papers/1204/1204.0232.pdf
+         *
+         * Reference to C++ thread commands:
+         * http://en.cppreference.com/w/c/thread
+         */
+
+        /**
+         * @todo Or Implement the Sequential Algorithm (Backup Plan):
+         * https://arxiv.org/ftp/arxiv/papers/1204/1204.0232.pdf
+         */
+
+        /**
+         * @todo Or Implement the Math-by-Hand Method (Last Resort):
+         * https://silentmatt.com/blog/2011/10/how-bigintegers-work/
+         */
+
+        return result;
+    }
+
+    BigInteger operator*(const BigInteger& lhs, const BigInteger& rhs) {
+        BigInteger result;
+
+        /**
+         * @todo Implement the Karatsuba Algorithm:
+         * https://brilliant.org/wiki/karatsuba-algorithm/
+         */
+
+        //Consider the SS Algorithm: https://tonjanee.home.xs4all.nl/SSAdescription.pdf
+        //SSA related: http://www.ams.org/publicoutreach/feature-column/fcarc-multiplication
+
+        return result;
+    }
+
+    Rational operator/(const BigInteger& lhs, const BigInteger& rhs) {
+
+        /*************************************************/
+        /*************************************************/
+        /****FIGURE OUT HOW TO IMPLEMENT THIS FUNCTION****/
+        /*************************************************/
+        /*************************************************/
+
+    }
+
+    BigInteger operator|(const BigInteger& lhs, const BigInteger& rhs) {
+        BigInteger result;
+
+        /**
+         * @todo Implement the Newton-Raphson Method
+         * https://en.m.wikipedia.org/wiki/Newton%27s_method
+         */
+
+        return result;
+    }
+
+    BigInteger operator%(const BigInteger& lhs, const BigInteger& rhs) {
+        BigInteger result;
+
+        /**
+         * @todo Implement the Newton-Raphson Method but with the remainder as the result
+         * https://en.m.wikipedia.org/wiki/Newton%27s_method
+         */
+
+        return result;
+    }
+
+    /**
+     * @todo Test the comparison operators
+     */
+
+    bool operator==(const BigInteger& lhs, const BigInteger& rhs) {
+        return lhs.value == rhs.value;
+    }
+
+    bool operator>(const BigInteger& lhs, const BigInteger& rhs) {
+        return lhs.value > rhs.value;
+    }
+
+    bool operator<(const BigInteger& lhs, const BigInteger& rhs) {
+        return lhs.value < rhs.value;
+    }
+
+    bool operator>=(const BigInteger& lhs, const BigInteger& rhs) {
+        return lhs.value >= rhs.value;
+    }
+
+    bool operator<=(const BigInteger& lhs, const BigInteger& rhs) {
+        return lhs.value <= rhs.value;
+    }
+
+    bool operator!=(const BigInteger& lhs, const BigInteger& rhs) {
+        return lhs.value != rhs.value;
+    }
+
+    std::string BigInteger::get_str_value() const {
+        return this->str_value;
+    }
+
+    std::vector<std::uint64_t> BigInteger::get_value() const {
+        return this->value;
+    }
+
+    bool BigInteger::get_sign() const {
+        return this->sign;
+    }
+
+} // namespace shaka
---
+++ tst/shaka_scheme/system/base/unit-BigInteger.cpp
@@ -0,0 +1,216 @@
+//
+// Created by Jared Shimabukuro on 3/7/18.
+//
+
+#include <gmock/gmock.h>
+#include "shaka_scheme/system/base/BigInteger.hpp"
+
+/**
+ * @brief Test: Default constructor for BigInteger
+ */
+TEST(BigInteger, test_default_constructor) {
+    // Given: a uint64_t vector assigned the value [0]
+    std::vector<std::uint64_t> num;
+    num.push_back(0);
+
+    // When: a BigInteger is constructed with no arguments
+    shaka::BigInteger n0;
+
+    // Then: the string value of the BigInteger should be equal to "0"
+    // & the value of the BigInteger should be equal to the given uint64_t vector
+    // which is equal to [0]
+    ASSERT_EQ(n0.get_str_value(), "0");
+    ASSERT_EQ(n0.get_value(), num);
+}
+
+/**
+ * @brief Test: Copy constructor for BigInteger
+ */
+TEST(BigInteger, test_copy_constructor) {
+    // Given: a BigInteger with
+    // string value = "280175720564098589193859415608762087"
+    // & value = [280175720564098589, 193859415608762087]
+    shaka::BigInteger n1 ("280175720564098589193859415608762087");
+
+    // When: a second BigInteger is constructed by copying the
+    // first BigInteger
+    shaka::BigInteger n2 (n1);
+
+    // Then: The second BigInteger is equal to the first BigInteger
+    ASSERT_EQ(n1, n2);
+}
+
+/**
+ * @brief Test: Constructor with String input of a positive integer for BigInteger
+ */
+TEST(BigInteger, test_string_constructor_positive) {
+    // Given: a uint64_t vector assigned the value
+    // [12, 345678909876543210]
+    std::vector<std::uint64_t> num;
+    num.push_back(12);
+    num.push_back(345678909876543210);
+
+    // When: a BigInteger is constructed constructed with the string input
+    // "12345678909876543210"
+    shaka::BigInteger n1 ("12345678909876543210");
+
+    // Then: The boolean value for the sign of the BigInteger should be false,
+    // The string value of the BigInteger is equal to "12345678909876543210",
+    // & the value of the BigInteger is equal to the given uint64_t vector
+    // which is equal to [12, 345678909876543210]
+    ASSERT_FALSE(n1.get_sign());
+    ASSERT_EQ(n1.get_str_value(), "12345678909876543210");
+    ASSERT_EQ(n1.get_value(), num);
+}
+
+/**
+ * @brief Test: Constructor with String input of a negative integer for BigInteger
+ */
+TEST(BigInteger, test_string_constructor_negative) {
+    // Given: a uint64_t vector assigned the value
+    // [987654321012, 345678909876543210]
+    std::vector<std::uint64_t> num;
+    num.push_back(987654321012);
+    num.push_back(345678909876543210);
+
+    // When: a BigInteger is constructed constructed with the string input
+    // "-987654321012345678909876543210"
+    shaka::BigInteger n1 ("-987654321012345678909876543210");
+
+    // Then: The boolean value for the sign of the BigInteger should be true,
+    // The string value of the BigInteger is equal to "-987654321012345678909876543210",
+    // & the value of the BigInteger is equal to the given uint64_t vector
+    // which is equal to [987654321012, 345678909876543210]
+    ASSERT_TRUE(n1.get_sign());
+    ASSERT_EQ(n1.get_str_value(), "-987654321012345678909876543210");
+    ASSERT_EQ(n1.get_value(), num);
+}
+
+/**
+ * @brief Test: Constructor with Vector input of a positive integer for BigInteger
+ */
+TEST(BigInteger, test_vector_constructor_positive) {
+    // Given: a boolean variable assigned the value 'false'
+    // & a uint64_t vector assigned the value
+    // [123456789012, 345678909876543210]
+    bool sign = false;
+    std::vector<std::uint64_t> num;
+    num.push_back(123456789012);
+    num.push_back(345678909876543210);
+
+    // When: a BigInteger is constructed using the given boolean variable
+    // & the given uint64_t vector
+    shaka::BigInteger n1 (sign, num);
+
+    // Then: the boolean value for the sign of the BigInteger should be equal
+    // to the given boolean variable which is equal to 'false',
+    // the value of the BigInteger should be equal to the value of the given
+    // uint64_t vector which is equal to [123456789012, 345678909876543210],
+    // & the string value of the BigInteger should be equal to
+    // "123456789012345678909876543210"
+    ASSERT_EQ(n1.get_sign(), sign);
+    ASSERT_EQ(n1.get_value(), num);
+    ASSERT_EQ(n1.get_str_value(), "123456789012345678909876543210");
+}
+
+/**
+ * @brief Test: Constructor with Vector input of a negative integer for BigInteger
+ */
+TEST(BigInteger, test_vector_constructor_negative) {
+    // Given: a boolean variable assigned the value 'true'
+    // & a uint64_t vector assigned the value
+    // [123456789012, 345678909876543210]
+    bool sign = true;
+    std::vector<std::uint64_t> num;
+    num.push_back(123456789012);
+    num.push_back(345678909876543210);
+
+    // When: a BigInteger is constructed using the given boolean variable
+    // & the given uint64_t vector
+    shaka::BigInteger n1 (sign, num);
+
+    // Then: the boolean value for the sign of the BigInteger should be equal
+    // to the given boolean variable which is equal to 'false',
+    // the value of the BigInteger should be equal to the value of the given
+    // uint64_t vector which is equal to [123456789012, 345678909876543210],
+    // & the string value of the BigInteger should be equal to
+    // "123456789012345678909876543210"
+    ASSERT_EQ(n1.get_sign(), sign);
+    ASSERT_EQ(n1.get_value(), num);
+    ASSERT_EQ(n1.get_str_value(), "-123456789012345678909876543210");
+}
+
+/**
+ * @brief Test: Constructors for BigInteger given a really, really big number
+ */
+TEST(BigInteger, test_really_really_big_number) {
+    // Given: a BigInteger constructed using a string of a really, really
+    // big negative integer
+    shaka::BigInteger n1 ("-1234567890123456789012345678901234567890"
+                          "1234567890123456789012345678901234567890"
+                          "1234567890123456789012345678901234567890"
+                          "1234567890123456789012345678901234567890"
+                          "1234567890123456789012345678901234567890"
+                          "1234567890123456789012345678901234567890"
+                          "1234567890123456789012345678901234567890"
+                          "1234567890123456789012345678901234567890"
+                          "1234567890123456789012345678901234567890"
+                          "1234567890123456789012345678901234567890"
+                          "1234567890123456789012345678901234567890"
+                          "12345678901234567890");
+
+    // Given: a Boolean variable assigned the value 'true' &
+    // a uint64_t vector assigned sections of a really, really big
+    // integer as its value
+    bool sign = true;
+    std::vector<std::uint64_t> bigNum;
+    bigNum.push_back(1234567890);
+    bigNum.push_back(123456789012345678);
+    bigNum.push_back(901234567890123456);
+    bigNum.push_back(789012345678901234);
+    bigNum.push_back(567890123456789012);
+    bigNum.push_back(345678901234567890);
+    bigNum.push_back(123456789012345678);
+    bigNum.push_back(901234567890123456);
+    bigNum.push_back(789012345678901234);
+    bigNum.push_back(567890123456789012);
+    bigNum.push_back(345678901234567890);
+    bigNum.push_back(123456789012345678);
+    bigNum.push_back(901234567890123456);
+    bigNum.push_back(789012345678901234);
+    bigNum.push_back(567890123456789012);
+    bigNum.push_back(345678901234567890);
+    bigNum.push_back(123456789012345678);
+    bigNum.push_back(901234567890123456);
+    bigNum.push_back(789012345678901234);
+    bigNum.push_back(567890123456789012);
+    bigNum.push_back(345678901234567890);
+    bigNum.push_back(123456789012345678);
+    bigNum.push_back(901234567890123456);
+    bigNum.push_back(789012345678901234);
+    bigNum.push_back(567890123456789012);
+    bigNum.push_back(345678901234567890);
+
+    // When: a second BigInteger is constructed by copying the values
+    // of the first BigInteger & a third BigInteger is constructed
+    // using the given Boolean value and the given uint64_t vector
+    shaka::BigInteger n2(n1);
+    shaka::BigInteger n3 (sign, bigNum);
+
+    // Then: the Boolean value of the sign of the first BigInteger should be
+    // equal to 'true' & the value of the first BigInteger should be equal
+    // to the given uint64_t vector
+    ASSERT_TRUE(n1.get_sign());
+    ASSERT_EQ(n1.get_value(), bigNum);
+
+    // Then: the first BigInteger should be equal to the second BigInteger
+    ASSERT_EQ(n1, n2);
+
+    // Then: the Boolean value of the sign of the third BigInteger should be
+    // equal to 'true', the value of the third BigInteger should be equal to
+    // the given uint64_t vector, & the string value of the third BigInteger
+    // should be equal to the string value of the first BigInteger
+    ASSERT_TRUE(n3.get_sign());
+    ASSERT_EQ(n3.get_value(), bigNum);
+    ASSERT_EQ(n3.get_str_value(), n1.get_str_value());
+}
