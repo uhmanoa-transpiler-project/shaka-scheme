bb285ed8c4d113917a4e0c25bc8d9647b95bb707
Austin Tasato <aytasato@gmail.com>
2018-04-19 15:01:20 -1000

    Added GC integration to parser, vector unit tests

--- tst/shaka_scheme/system/core/unit-core-vectors.cpp
+++ tst/shaka_scheme/system/core/unit-core-vectors.cpp
@@ -1,11 +1,20 @@
-#include <gmock/gmock.h>
+
+
 #include <shaka_scheme/system/core/vectors.hpp>
 #include <shaka_scheme/system/base/DataPair.hpp>
+
+#include <shaka_scheme/system/gc/GC.hpp>
+#include <shaka_scheme/system/gc/init_gc.hpp>
+
+#include <gmock/gmock.h>
 
 /**
  * @brief Test: Testing (list->vector l)
  */
 TEST(VectorsUnitTest, list_to_vector_3_elements) {
+  shaka::gc::GC garbage_collector;
+  shaka::gc::init_create_node(garbage_collector);
+
   using namespace shaka;
   const auto& c = shaka::create_node;
 
@@ -32,6 +41,9 @@ TEST(VectorsUnitTest, list_to_vector_3_elements) {
  * @brief Test: Testing (list->vector l)
  */
 TEST(VectorsUnitTest, vector_to_list_3_elements) {
+  shaka::gc::GC garbage_collector;
+  shaka::gc::init_create_node(garbage_collector);
+
   using namespace shaka;
   const auto& c = shaka::create_node;
 
